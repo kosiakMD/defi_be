0cd0c159b46a5d80932d8539dd5533967d314179
Smuta1991 <nigatif1991@gmail.com>
2021-03-22 04:41:07 +0300

    DAS-389 Create inital structure for Price Service

---
+++ price_service/src/models/asset_price.entity.ts
@@ -0,0 +1,28 @@
+
+import { Column, JoinColumn, Entity, PrimaryColumn, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+import Asset from './asset.entity';
+ 
+@Entity({name:'prices.asset_price'})
+class AssetPrice {
+  
+ 
+
+   
+  @PrimaryColumn()
+  public asset_id: number;
+ 
+  @Column()
+  public currency_id: number;
+
+  @Column()
+  public value: number;
+ 
+  @Column()
+  public timestamp: number;
+ 
+  @ManyToOne(() => Asset, (asset: Asset) => asset.asset_prices)
+  @JoinColumn({name : 'asset_id'})
+  public asset: Asset;
+}
+
+export default AssetPrice;

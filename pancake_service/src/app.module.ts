da2fd5f4d4ef7423ec9c0108b77e3626a9eb6138
Eugene <60896305+Eugennee@users.noreply.github.com>
2021-03-30 13:04:03 +0300

    Update pancake_service/src/app.module.ts
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- pancake_service/src/app.module.ts
+++ pancake_service/src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TerminusModule } from '@nestjs/terminus';
+
 import { PancakesModule } from './app/pancake.module';
 import { HealthController } from './health/health.controller';
 

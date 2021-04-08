44435d8ff4f667a1ad466ab44a01831eaf1943f3
Sergey Brezetskiy <sergey.brezetskiy@gmail.com>
2021-04-08 15:04:19 +0200

    Brezetsky/feature/price service performence (#48)
    * historical prices improvements
    
    * historical prices improvement
    
    * fix lint
    
    * price refactor
    
    * lint fixes
    
    * remove unsed interface

--- price_service/src/prices/prices.controller.ts
+++ price_service/src/prices/prices.controller.ts
@@ -1,16 +1,8 @@
-import { Controller, Get, Post, Query, Body } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
 import { ApiResponse } from '@nestjs/swagger';
 
 import { AssetPriceService } from '../services/asset_price.service';
 import { CurrentPrice, HistoricalPrice } from './interfaces/prices.interface';
-
-interface PriceHistoricalRequest {
-	addresses: string[];
-	timestamps: number[];
-	currencyId: number;
-	platformId: number;
-}
-
 
 @Controller('prices')
 export class PricesController {
@@ -19,85 +11,30 @@ export class PricesController {
 	@Get('/')
 	@ApiResponse({ status: 200, schema: {} })
 	async currentPrices(
-		@Query('platformId') platformId: number,
+		@Query('chainId') chainId: number,
 		@Query('currencyId') currencyId: number,
 		@Query('addresses') addresses: string[],
-	): Promise<CurrentPrice> {
-		if (!platformId) {
+		@Query('timestamps') timestamps: number[],
+	): Promise<CurrentPrice | HistoricalPrice> {
+		if (!chainId) {
 			return {};
 		}
 		if (!currencyId) {
 			return {};
 		}
-		if (!addresses) {
+		if (!addresses || !addresses.length) {
 			return {};
 		}
 
-		return await this.assetPriceService.getCurrent(addresses, currencyId, platformId);
-	}
+    if (timestamps || timestamps.length) {
+      return await this.assetPriceService.getHistoricalPrices(
+        addresses,
+        timestamps,
+        currencyId,
+        chainId,
+      ) as HistoricalPrice;
+    }
 
-	// TODO Docs body response
-	//@Get('/historical')
-	@Post('/historical')
-	async historicalPrices(
-		@Body() priceHistoricalRequest: PriceHistoricalRequest,
-	): Promise<HistoricalPrice> {
-		const { addresses, timestamps, currencyId, platformId } = priceHistoricalRequest;
-		if (
-			!platformId ||
-			!currencyId ||
-			!addresses ||
-			!addresses.length ||
-			!timestamps ||
-			!timestamps.length
-		) {
-			return {};
-		}
-
-		const response = {};
-
-		const timeInSec = Math.round(Date.now() / 1000);
-    const secondsInDay = 24 * 60 * 60;
-		const secondsInWeek = timeInSec - secondsInDay * 7;
-
-		// be sure that timesstamps has been sorted
-		timestamps.sort((a, b) => a - b);
-
-		const lastWeekTimestamps = [];
-		const historicalTimestamps = [];
-
-		for (let i = 0; i < timestamps.length; i++) {
-			timestamps[i] = timestamps[i] - (timestamps[i] % secondsInDay);
-
-			if (timestamps[i] > secondsInWeek) {
-				lastWeekTimestamps.push(timestamps[i]);
-			} else {
-				historicalTimestamps.push(timestamps[i]);
-			}
-		}
-
-		return Promise.all([
-			this.assetPriceService.getLastDaysHistoricalPrices(
-				addresses,
-				lastWeekTimestamps,
-				currencyId,
-				platformId,
-			),
-			this.assetPriceService.getHistoricalPrices(
-				addresses,
-				historicalTimestamps,
-				currencyId,
-				platformId,
-			)
-		]).then(items => {
-
-			addresses.forEach((item) => {
-				response[item] = Object.assign({}, items[0][item] || {}, items[1][item] || {});
-			});
-
-			return response as HistoricalPrice;
-			
-		  })
-		
+		return await this.assetPriceService.getCurrent(addresses, currencyId, chainId);
 	}
 }
--- price_service/src/services/asset_price.service.ts
+++ price_service/src/services/asset_price.service.ts
@@ -9,7 +9,7 @@ import { CurrentPrice, HistoricalPrice } from '../prices/interfaces/prices.inter
 export class AssetPriceService {
 	constructor(
 		@InjectRepository(AssetPrice) private readonly repo: Repository<AssetPrice>,
-		@InjectRepository(Asset) private readonly asset_repo: Repository<Asset>,
+		@InjectRepository(Asset) private readonly assetRepo: Repository<Asset>,
 	) {}
 
 	public async getAll() {
@@ -19,7 +19,7 @@ export class AssetPriceService {
 	public async getCurrent(
 		address: string[],
 		currencyId = 1,
-		platformId = 1,
+		chainId = 1,
 	): Promise<CurrentPrice> {
 		const entityManager = getManager();
 		const addressString = "('" + address.join("','") + "')";
@@ -30,11 +30,11 @@ export class AssetPriceService {
 		const timeInSec = Math.round(Date.now() / 1000) - FOURS_HOURS;
 
 		const querystr = `
-		SELECT a.*, ap.*
-		FROM prices.asset a
-		JOIN prices.asset_price ap ON ap.timestamp > ${timeInSec} AND (a.id = ap.asset_id)
-		LEFT OUTER JOIN prices.asset_price  ap2 ON (a.id = ap2.asset_id AND (ap.timestamp < ap2.timestamp ))
-		WHERE ap2.timestamp IS NULL AND a.address IN ${addressString} AND a.platform_id = ${platformId} AND ap.currency_id = ${currencyId}
+      SELECT a.*, ap.*
+      FROM prices.asset a
+      JOIN prices.asset_price ap ON ap.timestamp > ${timeInSec} AND (a.id = ap.asset_id)
+      LEFT OUTER JOIN prices.asset_price  ap2 ON (a.id = ap2.asset_id AND (ap.timestamp < ap2.timestamp ))
+      WHERE ap2.timestamp IS NULL AND a.address IN ${addressString} AND a.chain_id = ${chainId} AND ap.currency_id = ${currencyId}
 		`;
 
 		const dbEntities = await entityManager.query(querystr);
@@ -51,7 +51,7 @@ export class AssetPriceService {
 		address: string[],
 		timestamps: number[],
 		currencyId = 1,
-		platformId = 1,
+		chainId = 1,
 	): Promise<HistoricalPrice> {
 		const entityManager = getManager();
 
@@ -61,6 +61,9 @@ export class AssetPriceService {
 			response[item] = {};
 			sortedValues[item] = [];
 		});
+
+    // prepare timestamp in needed format
+    timestamps = this.formatTimeStamps(timestamps);
 
 		const addressString = "('" + address.join("','") + "')";
 		const timestampsString = '(' + timestamps.join(',') + ')';
@@ -69,81 +72,41 @@ export class AssetPriceService {
       SELECT a.*, ap.timestamp, ap.value
 		  FROM prices.asset a
 		  JOIN prices.asset_price ap ON (a.id = ap.asset_id)
-		  WHERE  ap.timestamp IN ${timestampsString} AND  a.address IN ${addressString} AND a.platform_id = ${platformId} AND ap.currency_id = ${currencyId}
+		  WHERE  ap.timestamp IN ${timestampsString} AND  a.address IN ${addressString} AND a.chain_id = ${chainId} AND ap.currency_id = ${currencyId}
       ORDER BY ap.asset_id, ap.timestamp
     `;
 
 		const dbEntities = await entityManager.query(querystr);
 		dbEntities.forEach((item) => {
-			response[item.address][item.timestamp + ''] = item.value;
+			response[item.address][item.timestamp.toString()] = item.value;
 			return;
 		});
 
 		return response as HistoricalPrice;
 	}
 
-	public async getLastDaysHistoricalPrices(
-		address: string[],
-		timestamps: number[],
-		currencyId = 1,
-		platformId = 1,
-	): Promise<HistoricalPrice> {
-		const entityManager = getManager();
+  formatTimeStamps(timestamps: number[]): number[] {
+    const timeInSec = Math.round(Date.now() / 1000);
+		const dayInSeconds = 24 * 60 * 60;
+		const hourInSeconds =  60 * 60;
+		const weekAgoInSeconds = timeInSec - dayInSeconds * 7;
 
-		const response = {};
-		const sortedValues = [];
-		address.forEach((item) => {
-			response[item] = {};
-			sortedValues[item] = [];
-		});
+		// be sure that timesstamps has been sorted
+		timestamps.sort((a, b) => a - b);
 
-		const addressString = "('" + address.join("','") + "')";
-		const SIDE_RANGE_SECONDS = 60 * 60 * 2;
-
-
-		const getClosestValue = (list, current) => {
-			return list.reduce(function(prev, curr) {
-				return (Math.abs(Number(curr.timestamp) - current) < Math.abs(Number(prev.timestamp) - current) ? curr : prev);
-			  });
+		const historicalTimestamps: number[] = [];
+		for (let i = 0; i < timestamps.length; i++) {
+			if (timestamps[i] > weekAgoInSeconds) {
+				timestamps[i] = timestamps[i] - (timestamps[i] % hourInSeconds);
+			} else {
+				timestamps[i] = timestamps[i] - (timestamps[i] % dayInSeconds);
+			}
+			historicalTimestamps.push(timestamps[i]);
 		}
 
-		let timestampsQueryString= '';
-		timestamps.forEach((item) => {
-			timestampsQueryString += '(ap.timestamp BETWEEN ' + (item - SIDE_RANGE_SECONDS) + ' AND ' + (item + SIDE_RANGE_SECONDS) +') OR ';
-			return;
-		});
+    return timestamps;
+  }
 
-		timestampsQueryString = timestampsQueryString.substring(0,timestampsQueryString.length -3);
-
-		const querystr = `
-      SELECT a.*, ap.timestamp, ap.value
-		  FROM prices.asset a
-		  JOIN prices.asset_price ap ON (a.id = ap.asset_id)
-		  WHERE   (${timestampsQueryString}) AND  a.address IN ${addressString} AND a.platform_id = ${platformId} AND ap.currency_id = ${currencyId}
-      ORDER BY ap.asset_id, ap.timestamp
-    `;
-
-		const dbEntities = await entityManager.query(querystr);
-		dbEntities.forEach((item) => {
-			sortedValues[item.address].push(item);
-			return;
-		});
-
-		address.forEach((item) => {
-			timestamps.forEach((tsItem) => {
-				if(sortedValues[item].length)
-					response[item][tsItem.toString()] = getClosestValue(sortedValues[item], Number(tsItem))['value'];
-				else
-					response[item][tsItem.toString()] = 0;
-				return;
-			});
-			return;
-		});
-
-		return response as HistoricalPrice;
-	}
-
-	
 
 }
 

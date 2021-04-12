b25ec5e0f41cfe1f895910bbf29f71f0ce0c527b
Anton Kosiak <kosiakmd@gmail.com>
2021-04-12 16:08:56 +0300

    Price Service Improvements
    Logger and Lint refactoring

--- price_service/src/app.module.ts
+++ price_service/src/app.module.ts
@@ -1,6 +1,13 @@
-import { Module } from '@nestjs/common';
+import { Inject, LoggerService, Module, OnModuleInit } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TerminusModule } from '@nestjs/terminus';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import {
+	utilities as nestWinstonModuleUtilities,
+	WINSTON_MODULE_NEST_PROVIDER,
+	WinstonModule,
+} from 'nest-winston';
+import * as winston from 'winston';
 
 import { configService } from './config/config.service';
 import { ExamplesModule } from './examples/example.module';
@@ -10,10 +17,57 @@ import { PricesModule } from './prices/prices.module';
 @Module({
 	controllers: [HealthController],
 	imports: [
+		ConfigModule.forRoot({
+			isGlobal: true,
+			envFilePath: [
+				'.env.development.local',
+				'.env.development',
+				'.env.production.local',
+				'.env.production',
+				'.env',
+			],
+		}),
+		WinstonModule.forRoot({
+			// options
+			level: process.env.LOG_LEVEL || 'info',
+			format: winston.format.json(),
+			defaultMeta: { service: process.env.SERVICE_NAME },
+			transports: [
+				// NestJS console like logs
+				new winston.transports.Console({
+					format: winston.format.combine(
+						winston.format.timestamp(),
+						nestWinstonModuleUtilities.format.nestLike(),
+					),
+				}),
+				// - Write all logs with level `error` and below to `error.log`
+				new winston.transports.File({ filename: process.env.LOG_ERROR_FILE, level: 'error' }),
+				// - Write all logs with level `info` and below to `combined.log`
+				new winston.transports.File({ filename: process.env.LOG_COMBINED_FILE }),
+			],
+		}),
 		TypeOrmModule.forRoot(configService.getTypeOrmConfig()),
 		PricesModule,
 		TerminusModule,
 		ExamplesModule,
 	],
 })
-export class AppModule {}
+export class AppModule implements OnModuleInit {
+	onModuleInit(): void {
+		const { SERVICE_NAME, PORT, HOST } = process.env;
+		this.logger.log(
+			{
+				name: SERVICE_NAME,
+				host: HOST,
+				port: PORT,
+			},
+			'SERVICE',
+		);
+		this.logger.verbose(this.configService, SERVICE_NAME);
+	}
+
+	constructor(
+		@Inject(WINSTON_MODULE_NEST_PROVIDER) private readonly logger: LoggerService,
+		private configService: ConfigService,
+	) {}
+}

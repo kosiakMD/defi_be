22c1281eef7cff9a71b2f74f058b5be2f509a7d3
vladdn <53942485+vladdn@users.noreply.github.com>
2021-05-22 00:56:36 +0300

    pricing for all bsc pools (#300)

--- integration_service/src/pancake/util/contants.ts
+++ integration_service/src/pancake/util/contants.ts
@@ -1,4 +1,4 @@
-export const PANCAKE_PROJECT: string = 'pancake'
+export const PANCAKE_PROJECT: string = 'Pancake V1'
 export const PANCAKE_V2_PROJECT: string = 'Pancake V2'
 export const BUSD_BNB_PAIR_ADDRESS = '0x1b96b92314c44b159149f7e0303511fb2fc4774f';
 export const USDT_BNB_PAIR_ADDRESS = '0x20bcc3b8a0091ddac2d0bc30f68e6cbb97de59cd';

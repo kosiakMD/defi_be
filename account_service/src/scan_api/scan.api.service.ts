bfae49141b88f75cdcf700f503123baa88553073
Sergey Brezetskiy <sergey.brezetskiy@gmail.com>
2021-06-25 12:01:59 +0200

    fix issue with transactions (#375)

--- account_service/src/scan_api/scan.api.service.ts
+++ account_service/src/scan_api/scan.api.service.ts
@@ -280,7 +280,9 @@ export class ScanApiService {
     }
 
     transactions.forEach((tx) => {
-      const price = prices.prices[this.mainCoinAddress][tx.timeStamp];
+      const price = prices.prices[this.mainCoinAddress]
+        ? prices.prices[this.mainCoinAddress][tx.timeStamp]
+        : 0;
       const valueUSD = totalPrice(tx.value.toString(), price, 18);
       const feeUSD = transactionFeeUSD(tx.gasPrice, tx.gasUsed, 18, price);
       Object.assign(tx, {

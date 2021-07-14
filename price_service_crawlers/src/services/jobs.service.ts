b5478c1eab80e44f96138352b49540e7914ca424
Anton Kosiak <kosiakmd@gmail.com>
2021-07-14 13:25:35 +0200

    Revert "ID-1471 (#400)" (#401)
    This reverts commit 9ebcb70f584c60b96b758650b95bd69bd0e263b5.

--- price_service_crawlers/src/services/jobs.service.ts
+++ price_service_crawlers/src/services/jobs.service.ts
@@ -119,18 +119,14 @@ export class JobsService {
           {},
         );
 
-        //SUSHI
-        await cancel('CRAWL_SUSHI_CURRENT_PRICE');
-        this.agenda.define(
-          'CRAWL_SUSHI_CURRENT_PRICE',
-          { lockLifetime: 10000 },
-          this.sushiswapJob.getCurrentPrices.bind(this),
-        );
-        this.agenda.every(
-          CURRENT_PRICE_SECONDS_INTERVAL + ' seconds',
-          'CRAWL_SUSHI_CURRENT_PRICE',
-          {},
-        );
+        // //SUSHI
+        // await cancel('CRAWL_SUSHI_CURRENT_PRICE');
+        // this.agenda.define(
+        //   'CRAWL_SUSHI_CURRENT_PRICE',
+        //   { lockLifetime: 10000 },
+        //   this.sushiswapJob.getCurrentPrices.bind(this),
+        // );
+        // this.agenda.every(CURRENT_PRICE_SECONDS_INTERVAL + ' seconds', 'CRAWL_SUSHI_CURRENT_PRICE', {});
 
         await cancel('CRAWL_SUSHI_NEW_TOKENS_HISTORY');
         this.agenda.define(
@@ -145,18 +141,18 @@ export class JobsService {
         );
 
         // //UNI
-        await cancel('CRAWL_UNISWAP_CURRENT_PRICE');
-        this.logger.log('starting sushi');
-        this.agenda.define(
-          'CRAWL_UNISWAP_CURRENT_PRICE',
-          { lockLifetime: 10000 },
-          this.uniswapJob.getCurrentPrices.bind(this),
-        );
-        this.agenda.every(
-          CURRENT_PRICE_SECONDS_INTERVAL + ' seconds',
-          'CRAWL_UNISWAP_CURRENT_PRICE',
-          {},
-        );
+        // await cancel('CRAWL_UNISWAP_CURRENT_PRICE');
+        // this.logger.log('starting sushi');
+        // this.agenda.define(
+        //   'CRAWL_UNISWAP_CURRENT_PRICE',
+        //   { lockLifetime: 10000 },
+        //   this.uniswapJob.getCurrentPrices.bind(this),
+        // );
+        // this.agenda.every(
+        //   CURRENT_PRICE_SECONDS_INTERVAL + ' seconds',
+        //   'CRAWL_UNISWAP_CURRENT_PRICE',
+        //   {},
+        // );
 
         await cancel('CRAWL_UNISWAP_NEW_TOKENS_HISTORY');
         this.agenda.define(

1b67bdb89d054d5fdf46edab77335b4aaa7553e5
Ivan <kolesnikivan1002@gmail.com>
2021-07-23 00:24:27 +0300

    ID-1303 Add cache for pools queries (#408)
    Scope:
    - integration service
    
    - Added cache for getProjectPools method
    - Updated .env.example
    - Updated env validation

--- integration_service/src/pools/pools.service.ts
+++ integration_service/src/pools/pools.service.ts
@@ -1,5 +1,7 @@
-import { Injectable } from '@nestjs/common';
+import { CACHE_MANAGER, Inject, Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { InjectRepository } from '@nestjs/typeorm';
+import { Cache } from 'cache-manager';
 
 import { LiquidityPoolsEntity } from './entities/liquidity.pools.entity';
 import {
@@ -16,12 +18,24 @@ import { LiquidityPoolsRepository } from './repository/liquidity.pools.repositor
 
 @Injectable()
 export class PoolsService {
+  cacheTTLInSeconds: number;
+
   constructor(
+    private readonly config: ConfigService,
+    @Inject(CACHE_MANAGER) private readonly cache: Cache,
     @InjectRepository(LiquidityPoolsEntity)
     private readonly liquidityPoolsRepository: LiquidityPoolsRepository,
-  ) {}
+  ) {
+    this.cacheTTLInSeconds = config.get<number>('POOLS_CACHE_TTL_IN_SECONDS') || 15 * 60;
+  }
 
   async getPoolsToDisplay(): Promise<LiquidityPoolsEntity[]> {
+    const cachedPools = await this.getCachedPools();
+
+    if (cachedPools?.length) {
+      return cachedPools;
+    }
+
     const dbPools = await this.liquidityPoolsRepository
       .createQueryBuilder('pools')
       .orWhere('pools.updated_at = (select max(updated_at) from liquidity_pools)')
@@ -48,13 +62,39 @@ export class PoolsService {
       return p.project === PROJECT_PANCAKE_V2 && p.reserveUsd > PANCAKE_MIN_RESERVE;
     });
 
-    return [...uniswapPools, ...sushiswapPools, ...pancakePools, ...pancakeV2Pools];
+    const allPools = [...uniswapPools, ...sushiswapPools, ...pancakePools, ...pancakeV2Pools];
+
+    this.updateCachePools(allPools);
+
+    return allPools;
   }
 
   async getProjectPools(project: string): Promise<LiquidityPoolsEntity[]> {
-    return this.liquidityPoolsRepository
+    const cachedPools = await this.getCachedPools(project);
+
+    if (cachedPools?.length) {
+      return cachedPools;
+    }
+
+    const dbPools = await this.liquidityPoolsRepository
       .createQueryBuilder('pools')
       .where('pools.project = :project', { project: project })
       .getMany();
+
+    this.updateCachePools(dbPools, project);
+
+    return dbPools;
+  }
+
+  private async getCachedPools(seed?: string): Promise<LiquidityPoolsEntity[]> {
+    return await this.cache.get<LiquidityPoolsEntity[]>(this.getCacheKey(seed));
+  }
+
+  private async updateCachePools(pools: LiquidityPoolsEntity[], seed?: string): Promise<void> {
+    this.cache.set(this.getCacheKey(seed), pools, { ttl: this.cacheTTLInSeconds });
+  }
+
+  private getCacheKey(seed?: string): string {
+    return `pools_${seed}`;
   }
 }

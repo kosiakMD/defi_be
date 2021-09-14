2cd9edb5ea183e755cea2182c5c80bc5e17ffe99
Roman <46167139+RomanHreits@users.noreply.github.com>
2021-09-14 12:35:21 +0300

    - added isDataPresent field to assets.entity (#534)

--- account_service_consumers/apps/migration_consumer/src/store/entities/assets.entity.ts
+++ account_service_consumers/apps/migration_consumer/src/store/entities/assets.entity.ts
@@ -28,4 +28,7 @@ export class AssetsEntity {
 
   @Column({ name: 'project_id', nullable: true })
   projectId: boolean;
+
+  @Column({ name: 'is_data_present', nullable: true })
+  isDataPresent: boolean;
 }

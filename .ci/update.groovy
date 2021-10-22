430d052e6895a3c6bc9c7c12acce1278f6b24029
Petro Sidlovskyy <sidlovskyy@gmail.com>
2021-10-22 19:11:42 +0300

    Merge master to dev (#659)
    * Merge master to dev
    
    * Fix build

---
+++ .ci/update.groovy
@@ -0,0 +1,91 @@
+#!/usr/bin/env groovy
+
+pipeline {
+    agent any
+    parameters {
+        run(name: "DEPLOYMENT", description: "Stack deployment to update", projectName: "/deployments/deploy-stack", filter: "SUCCESSFUL")
+    }
+    options {
+        buildDiscarder(
+            logRotator(
+                numToKeepStr: "30",
+                daysToKeepStr: "30",
+                artifactNumToKeepStr: "30",
+                artifactDaysToKeepStr: "30"
+            )
+        )
+    }
+    environment {
+        ENVIRONMENT     = ""
+        STACK_ID        = ""
+        OWNER           = ""
+        BRANCH          = ""
+    }
+    stages {
+        stage("Get Stack") {
+            steps {
+                script {
+                    def DEPLOY_JOB_RUN = Jenkins \
+                        .instance \
+                        .getItemByFullName("/deployments/deploy-stack") \
+                        .getBuild(env.DEPLOYMENT.split("/").last())
+
+                    (ENVIRONMENT, STACK_ID) = DEPLOY_JOB_RUN \
+                        .getDisplayName() \
+                        .split(" - ") \
+                        .last() \
+                        .split("-", 2)
+
+                    OWNER = (DEPLOY_JOB_RUN.getCause(Cause.UserIdCause) ?: DEPLOY_JOB_RUN.getCause(Cause.UpstreamCause).getUpstreamCauses()[0]) \
+                        .getUserId()
+
+                    FRONTEND_BRANCH_NAME = DEPLOY_JOB_RUN.getDisplayName().split(" - ")[0]
+                    BACKEND_BRANCH_NAME  = DEPLOY_JOB_RUN.getDisplayName().split(" - ")[1]
+
+                    currentBuild.displayName = "${ENVIRONMENT}-${STACK_ID}"
+                }
+            }
+        }
+        stage("Approval") {
+            steps {
+                timeout(time: 30, unit: "MINUTES") {
+                    input(
+                        message: "Update deployment ${STACK_ID} in ${ENVIRONMENT} environment?\n\nWaiting for approval from ${OWNER}",
+                        ok: "Update",
+                        submitter: OWNER
+                    )
+                }
+            }
+        }
+        stage("Update") {
+            steps {
+                script {
+                    build(
+                        job: "/deployments/deploy-stack",
+                        wait: true,
+                        propagate: true,
+                        parameters: [
+                            string(name: "ENVIRONMENT", value: ENVIRONMENT),
+                            gitParameter(name: "BACKEND_BRANCH_NAME", value: BACKEND_BRANCH_NAME),
+                            gitParameter(name: "FRONTEND_BRANCH_NAME", value: FRONTEND_BRANCH_NAME),
+                            [ $class: "WHideParameterValue", name: "STACK_ID", value: STACK_ID ]
+                        ]
+                    )
+                }
+            }
+            post {
+                success {
+                    catchError(buildResult: "SUCCESS", stageResult: "SUCCESS") {
+                        script {
+                            Jenkins \
+                                .instance \
+                                .getItemByFullName("/deployments/deploy-stack") \
+                                .getBuild(env.DEPLOYMENT.split("/").last()) \
+                                .delete()
+                        }
+                    }
+                }
+            }
+        }
+    }
+}

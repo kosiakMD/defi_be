2b577cb8aa0fd650a5ab3ccae13996174e5e7ccf
Ivan <kolesnikivan1002@gmail.com>
2021-11-24 17:11:40 +0200

    [ID-2563] QuickSwap for several wallets. Integration V2 (#729)
    * Implemented QuickSwap on API V2

--- apps/integration_service/src/protocol/protocols/quickswap/quickswapProtocol.ts
+++ apps/integration_service/src/protocol/protocols/quickswap/quickswapProtocol.ts
@@ -1,13 +1,21 @@
+import { BaseData } from 'apps/integration_service/src/interfaces/transactions.interfaces';
 import BigNumber from 'bignumber.js';
 import { plainToClass } from 'class-transformer';
 
 import { Inject, Injectable } from '@nestjs/common';
 import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
 
-import { Address, ChainDto, IncomeLiquidityPosition, Logger, PoolTokenDto } from '@app/common';
+import {
+  Address,
+  ChainDto,
+  IncomeLiquidityPosition,
+  Logger,
+  PoolTokenDto,
+  ProtocolTypeEnum,
+} from '@app/common';
 import { FeatureEnum } from '@app/common';
 import { IntegrationClaimableTokenDto } from '@app/common';
-import { BaseData } from '@app/common/dto/BaseData';
+import { BaseDataStaking } from '@app/common/dto/base.data.staking.dto';
 import {
   ChainAbbrEnum,
   ChainIdEnum,
@@ -19,7 +27,11 @@ import { mapToObject } from '@app/common/utils/object';
 import { toChunkedArray } from '@app/common/utils/transform';
 import { Web3ProviderService } from '@app/common/web3provider';
 
-import { IntegrationStakingPositionDto, LPToken } from '../../../integrations/integrations.dto';
+import {
+  IntegrationERC20TokenDto,
+  IntegrationStakingPositionDto,
+  LPToken,
+} from '../../../integrations/integrations.dto';
 import { AccountService } from '../../../microservices/account.service';
 import { PriceService } from '../../../microservices/price.service';
 import { PairDto } from '../../../subgraph';
@@ -231,6 +243,166 @@ export class QuickswapProtocol extends DataProviderProtocol implements AbstractP
       this.logger.error(error, 'QuickswapService.getDataByAddress');
       throw error;
     }
+  }
+
+  public async getStakingPositionsV2(addresses: Address[], chain: ChainDto): Promise<BaseData[]> {
+    const baseData: BaseDataStaking[] = [];
+
+    const { data: rawRewardTokens } = await this.accountService.getAssets(
+      [QUICKSWAP_REWARDS_TOKEN_ADDRESS],
+      [ChainIdEnum.plg],
+    );
+    const rawRewardToken = rawRewardTokens[0];
+
+    const { prices } = await this.priceService.getTokenPricesFetch(
+      [rawRewardToken.address],
+      ChainIdEnum.plg,
+    );
+
+    const rewardTokenPrice = prices[rawRewardToken.address];
+
+    await Promise.all(
+      addresses.map(async (userAddress) => {
+        const stakingTokensBalances = await this.multicall.getBalancesOf(
+          QUICKSWAP_STAKING_CONTRACTS.map(({ stakingContractAddress }) => stakingContractAddress),
+          userAddress,
+        );
+
+        const stakingTokensClaimable = await this.multicall.getEarned(
+          QUICKSWAP_STAKING_CONTRACTS.map(({ stakingContractAddress }) => stakingContractAddress),
+          userAddress,
+          QUICKSWAP_STAKING_REWARDS_ABI,
+        );
+
+        const lpStakingTokens = await this.multicall.getStakingTokens(
+          QUICKSWAP_STAKING_CONTRACTS.map(({ pairAddress }) => pairAddress),
+          QUICKSWAP_STAKING_TOKEN_ABI,
+        );
+
+        const stakingPairsData = await this.getSubgraphPairs(
+          Object.keys(mapToObject(lpStakingTokens)).flat(),
+        );
+
+        const stakingTokens = new Map<string, PairDto>();
+
+        stakingPairsData.forEach((pairData) => {
+          stakingTokens.set(pairData.id, pairData);
+        });
+
+        const stakingPositions = await Promise.all(
+          QUICKSWAP_STAKING_CONTRACTS.map(
+            async ({
+              pairAddress,
+              stakingContractAddress,
+            }): Promise<IntegrationStakingPositionDto> => {
+              const pairData = stakingTokens.get(pairAddress);
+
+              const balance = new BigNumber(
+                stakingTokensBalances.get(stakingContractAddress.toLocaleLowerCase()),
+              )
+                .div(decimalsDivider(rawRewardToken.decimals))
+                .toString();
+              const claimable = stakingTokensClaimable.get(
+                stakingContractAddress.toLocaleLowerCase(),
+              );
+
+              const claimableDataBalance = new BigNumber(claimable) //
+                .div(decimalsDivider(rawRewardToken.decimals))
+                .toString();
+
+              const poolShare = new BigNumber(balance) //
+                .div(pairData.totalSupply)
+                .toNumber();
+
+              const rewardToken = plainToClass(IntegrationClaimableTokenDto, {
+                address: QUICKSWAP_REWARDS_TOKEN_ADDRESS,
+                name: rawRewardToken.name,
+                symbol: rawRewardToken.symbol,
+                decimals: rawRewardToken.decimals,
+                totalSupply: rawRewardToken.totalSupply,
+                price: rewardTokenPrice,
+                claimableData: {
+                  balance: claimableDataBalance,
+                  value: new BigNumber(claimableDataBalance) //
+                    .times(rewardTokenPrice)
+                    .toString(),
+                },
+              });
+
+              const stakingToken = plainToClass(IntegrationERC20TokenDto, {
+                address: pairAddress,
+                name: 'Uniswap V2',
+                symbol: 'UNI-V2',
+                decimals: 18,
+                tokens: this.getLPTokens(pairData, poolShare),
+              });
+
+              return plainToClass(IntegrationStakingPositionDto, {
+                address: null,
+                poolId: null,
+                poolName: null,
+                staked: balance,
+                rewardToken,
+                stakingToken,
+                rewards: [rewardToken],
+              });
+            },
+          ),
+        );
+
+        baseData.push(
+          plainToClass(BaseDataStaking, {
+            chain: chain,
+            userAddress,
+            items: stakingPositions.filter(({ staked }) => +staked),
+            feature: FeatureEnum.staking,
+            projectName: ProjectEnum.quickswap,
+            protocolType: ProtocolTypeEnum.staking,
+          }),
+        );
+      }),
+    );
+
+    return baseData;
+  }
+
+  public async getFeatureData(
+    addresses: Address[],
+    chain: ChainDto,
+    feature: FeatureEnum,
+  ): Promise<BaseData[]> {
+    switch (feature) {
+      case FeatureEnum.staking:
+        return this.getStakingPositionsV2(addresses, chain);
+      case FeatureEnum.pools:
+        return [];
+      default:
+        return [];
+    }
+  }
+
+  public async getAllFeaturesBaseData(
+    addresses: Address[],
+    chain: ChainDto,
+  ): Promise<[BaseData[], string[]]> {
+    const chainFeatures = await Promise.allSettled(
+      this.features[chain.abbr].map((f) => {
+        return this.getFeatureData(addresses, chain, f);
+      }),
+    );
+
+    const data = [];
+    const errors = [];
+    chainFeatures.forEach((r) => {
+      if (r.status === 'fulfilled') {
+        data.push(r.value);
+      } else {
+        this.logger.error(r.reason, r.reason.stack, QuickswapProtocol.name);
+        errors.push(r.reason.toString());
+      }
+    });
+
+    return [data.flat(), errors];
   }
 }
 

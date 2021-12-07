ce63a67e33f9d418ed335f5a44c343c0909f2cd4
vladdn <vlad.d.essentia@gmail.com>
2021-12-07 23:30:06 +0200

    solana web3 provider

---
+++ libs/common/src/web3provider/web3.solana.provider.service.ts
@@ -0,0 +1,23 @@
+import { Connection } from '@solana/web3.js';
+
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+
+import { ChainIdEnum } from '..';
+
+@Injectable()
+export class Web3SolanaProviderService {
+  private readonly providers = new Map<ChainIdEnum, Connection>();
+
+  constructor(private readonly configService: ConfigService) {
+    this.setProvider(ChainIdEnum.sol, 'SOL_URL');
+  }
+
+  public getInstanceByChainId(chain: ChainIdEnum): Connection {
+    return this.providers.get(chain);
+  }
+
+  private setProvider(chain: ChainIdEnum, env: string) {
+    this.providers.set(chain, new Connection(this.configService.get(env)));
+  }
+}

8a11ee84b16990adb79017836e9ba949e9f5d922
vladdn <vlad.d.essentia@gmail.com>
2021-12-09 16:23:29 +0200

    raydiym balances

--- apps/integration_service/src/protocol/protocols/raydium/raydium.staking.ts
+++ apps/integration_service/src/protocol/protocols/raydium/raydium.staking.ts
@@ -1,39 +1,86 @@
+import { Connection } from '@solana/web3.js';
+import { BigNumber as BN } from 'bignumber.js';
+import { Cache } from 'cache-manager';
 import { plainToClass } from 'class-transformer';
 
-import { Injectable } from '@nestjs/common';
+import { CACHE_MANAGER, Inject, Injectable } from '@nestjs/common';
+import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
 
-import { Address, ChainDto, FeatureEnum, ProtocolTypeEnum } from '@app/common';
+import {
+  Address,
+  ChainDto,
+  FeatureEnum,
+  Logger,
+  ProtocolTypeEnum,
+  RaydiumProtocolEnum,
+} from '@app/common';
 import { BaseDataStaking } from '@app/common/dto/base.data.staking.dto';
+import { NotifyStaking } from '@app/common/jobs/notify.dto';
+import { IntegrationStakingPositionDto } from '@app/common/jobs/staking';
 
-import { getListInfo } from './utils/staking/staking-pools'
 import { Web3Provider } from '../../../chain/web3.provider';
-import { Connection } from '@solana/web3.js';
-
+import { decimalsDivider } from '../../../utils/util';
+import { getListInfo } from './utils/staking/staking-pools';
 
 @Injectable()
 export class RaydiumStaking {
+  private web3: Connection;
+
   constructor(
+    @Inject(WINSTON_MODULE_NEST_PROVIDER) private readonly logger: Logger,
+    @Inject(CACHE_MANAGER) private readonly cache: Cache,
     private readonly web3Provider: Web3Provider,
-  ) {}
+  ) {
+    this.web3 = web3Provider.instanceSol();
+  }
 
   public async getData(addresses: Address[], chain: ChainDto): Promise<BaseDataStaking[]> {
-    
+    const cacheKey = `${chain.id}_${RaydiumProtocolEnum.raydium}_${FeatureEnum.staking}`;
+    const cachedPools: NotifyStaking = await this.cache.get(cacheKey);
+    if (!cachedPools) {
+      throw new Error(`not found cached data for key '${cacheKey}'`);
+    }
+    const cachedPoolsMap = new Map<string, IntegrationStakingPositionDto>(
+      cachedPools.items.map((cpi) => [cpi.address.toString(), cpi]),
+    );
+    console.log(cachedPoolsMap);
+
     const programId = 'EhhTKczWMGQt46ynNeRX1WfeagwwJd7ufHvCDjRxjo5Q';
-    const web3: Connection = this.web3Provider.instanceSol();
-    
-    const stakeAccounts = await getListInfo(web3, addresses[0], programId) || [];
 
-    console.log(stakeAccounts);
+    const baseDataStakingMap: Map<string, BaseDataStaking> = new Map<string, BaseDataStaking>(
+      addresses.map((a) => [
+        a,
+        plainToClass(BaseDataStaking, {
+          chain: chain,
+          userAddress: a,
+          protocolType: ProtocolTypeEnum.staking,
+          projectName: RaydiumProtocolEnum.raydium,
+          feature: FeatureEnum.staking,
+          items: [],
+        }),
+      ]),
+    );
 
-    return [
-      plainToClass(BaseDataStaking, {
-        chain: chain,
-        userAddress: addresses[0],
-        protocolType: ProtocolTypeEnum.staking,
-        projectName: 'ray',
-        items: [],
-        feature: FeatureEnum.staking,
-      }),
-    ];
+    const balances = await getListInfo(this.web3, addresses, programId);
+    for (const b of balances) {
+      const vault = cachedPoolsMap.get(b.poolId);
+      // tooo: add logging
+      if (!vault) {
+        continue;
+      }
+      const stakingPosition: IntegrationStakingPositionDto = vault;
+      const balance = new BN(b.depositBalance).div(decimalsDivider(vault.stakingToken.decimals));
+      const userShare = balance.toNumber() / vault.stakingToken.totalSupply;
+      stakingPosition.staked = balance.toNumber();
+      stakingPosition.stakingToken.tokens.forEach((t) => {
+        t.balance = userShare * t.reserve;
+        t.value = t.price * t.balance;
+      });
+      baseDataStakingMap
+        .get('3eX8d6SKEqQRPcjpNc3xrbrWKjGzvVkmXbP1JS6ioFEk')
+        .items.push(stakingPosition);
+    }
+
+    return Array.from(baseDataStakingMap.values());
   }
 }
--- apps/integration_service/src/protocol/protocols/raydium/utils/staking/staking-pools.ts
+++ apps/integration_service/src/protocol/protocols/raydium/utils/staking/staking-pools.ts
@@ -1,75 +1,88 @@
-import { USER_STAKE_INFO_ACCOUNT_LAYOUT_V4 } from './structures'
 import { PublicKey, Connection } from '@solana/web3.js';
+
 import { Address, ChainDto, FeatureEnum, ProtocolTypeEnum } from '@app/common';
+
 import { offset } from './buffer-layout';
+import { USER_STAKE_INFO_ACCOUNT_LAYOUT_V4 } from './structures';
 
 export interface getFilteredProgramAccountsResults {
-    publicKey: PublicKey,
-    accountInfo: {
-      data: Buffer,
-      executable: string,
-      owner: PublicKey,
-      lamports: string
-    }
-  }
+  publicKey: PublicKey;
+  accountInfo: {
+    data: Buffer;
+    executable: string;
+    owner: PublicKey;
+    lamports: string;
+  };
+}
 
 export interface stakeAccount {
-    poolId: string,
-    depositBalance: number,
-    rewardDebt: number,
-    rewardDebtB: number,
-    stakeAccountAddress: string
-  }
+  poolId: string;
+  depositBalance: number;
+  rewardDebt: number;
+  rewardDebtB: number;
+  stakeAccountAddress: string;
+}
 
 export const getFilteredProgramAccounts = async (opts: {
-    connection: Connection,
-    address: Address, 
-    offset?: number,
-    encoding?: string,
-    programId: PublicKey 
-  }): Promise<getFilteredProgramAccountsResults[]> => {
-     
-    const offset = opts.offset || 40; 
-    const encoding = opts.encoding || 'base64';
-    //@ts-ignore
-    const resp = await opts.connection._rpcRequest('getProgramAccounts', [
+  connection: Connection;
+  addresses: Address[];
+  offset?: number;
+  encoding?: string;
+  programId: PublicKey;
+}): Promise<getFilteredProgramAccountsResults[]> => {
+  const offset = opts.offset || 40;
+  const encoding = opts.encoding || 'base64';
+  const addressesFilters = opts.addresses.map((a) => {
+    return {
+      memcmp: {
+        bytes: new PublicKey(a).toBase58(),
+        offset,
+      },
+    };
+  });
+  //@ts-ignore
+  const resp = await opts.connection._rpcRequest('getProgramAccounts', [
     new PublicKey(opts.programId).toBase58(),
-        {
-            filters: [ 
-                {
-                    memcmp: {
-                        bytes: new PublicKey(opts.address).toBase58(),
-                        offset
-                    }
-                }
-            ],
-            encoding
-        }
-    ])
+    {
+      filters: addressesFilters,
+      encoding,
+    },
+  ]);
 
-    return await resp.result.map(({ pubkey, account: { data, executable, owner, lamports } }): getFilteredProgramAccountsResults => ({
-        publicKey: new PublicKey(pubkey),
-        accountInfo: {
-          data: Buffer.from(data[0], 'base64'),
-          executable,
-          owner: new PublicKey(owner),
-          lamports
-        }
-      }))
-}
+  return await resp.result.map(
+    ({
+      pubkey,
+      account: { data, executable, owner, lamports },
+    }): getFilteredProgramAccountsResults => ({
+      publicKey: new PublicKey(pubkey),
+      accountInfo: {
+        data: Buffer.from(data[0], 'base64'),
+        executable,
+        owner: new PublicKey(owner),
+        lamports,
+      },
+    }),
+  );
+};
 
 export const getBigNumber = (num: any) => {
-    return num === undefined || num === null ? 
-        0 : parseFloat(num.toString())
-}
+  return num === undefined || num === null ? 0 : parseFloat(num.toString());
+};
 
-export const getListInfo = async (connection: Connection, address: Address, idProgram: string) =>{
+export const getListInfo = async (
+  connection: Connection,
+  addresses: Address[],
+  idProgram: string,
+) => {
   const programId = new PublicKey(idProgram);
-  const accountInfos = await getFilteredProgramAccounts({connection, address: address, programId}); 
-  let stakeAccounts = [];
+  const accountInfos = await getFilteredProgramAccounts({
+    connection,
+    addresses: addresses,
+    programId,
+  });
+  const stakeAccounts = [];
 
   for (const accountInfo of accountInfos) {
-
     const { data } = accountInfo.accountInfo;
     const LAYOUT = USER_STAKE_INFO_ACCOUNT_LAYOUT_V4;
     const userStakeInfo = LAYOUT.decode(data);
@@ -78,17 +91,14 @@ export const getListInfo = async (connection: Connection, address: Address, idPr
     const rewardDebtB = getBigNumber(userStakeInfo.rewardDebtB);
     const depositBalance = getBigNumber(userStakeInfo.depositBalance);
 
-    const decimal = 6;
-
     stakeAccounts.push({
       poolId,
-      depositBalance: depositBalance / 10 ** decimal,
-      rewardDebt: rewardDebt / 10 ** decimal,
-      rewardDebtB: rewardDebtB / 10 ** decimal,
-      stakeAccountAddress: accountInfo.publicKey.toBase58()
-    })
-
+      depositBalance: depositBalance,
+      rewardDebt: rewardDebt,
+      rewardDebtB: rewardDebtB,
+      stakeAccountAddress: accountInfo.publicKey.toBase58(),
+    });
   }
 
   return stakeAccounts;
-}
+};

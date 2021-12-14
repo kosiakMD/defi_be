f4a420167e688777dc704cb369002317adad3d09
Reed <86171134+not-reed@users.noreply.github.com>
2021-12-14 05:09:42 -0800

    lowercased hardcoded aave token (#783)
    Co-authored-by: Anton Kosiak <kosiakmd@gmail.com>

--- jobs/lambda_protocol_prices/src/jobs/protocols/aave/tokens.ts
+++ jobs/lambda_protocol_prices/src/jobs/protocols/aave/tokens.ts
@@ -7,7 +7,7 @@ export const ADDITIONAL_TOKENS: Partial<Record<ChainIdEnum, IAaveGenericToken[]>
     {
       // stkAave => aave
       underlyingAssetAddress: '0x7fc66500c84a76ad7e9c93437bfc5ac33e2ddae9',
-      id: '0x4da27a545c0c5B758a6BA100e3a049001de870f5',
+      id: '0x4da27a545c0c5b758a6ba100e3a049001de870f5',
     },
   ],
 };

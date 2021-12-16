aa8d62cacb186bd5514534ae906b704cbfb1b779
sidlovskyy <sidlovskyy@gmail.com>
2021-12-16 11:05:18 +0200

    Add blacklist of matic token

--- jobs/assets/src/utils/blacklisted.ts
+++ jobs/assets/src/utils/blacklisted.ts
@@ -1,4 +1,5 @@
 // TODO: This one is temporary solution, we should keep it in database
 export const blacklisted = [
-  '0x4922a015c4407f87432b179bb209e125432e4a2a'
+  '0x4922a015c4407f87432b179bb209e125432e4a2a', // Gold Tether
+  '0x0000000000000000000000000000000000001010', // Matic Token
 ];

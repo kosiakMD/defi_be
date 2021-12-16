8b475fd12986db8c387d2354c7f6811432e06b52
vladdn <vlad.d.essentia@gmail.com>
2021-12-16 16:39:32 +0200

    integration

--- apps/integration_service/src/modules/subgraphs/subgraphs/uniswap.v3.subgraph.ts
+++ apps/integration_service/src/modules/subgraphs/subgraphs/uniswap.v3.subgraph.ts
@@ -1,6 +1,7 @@
 import { map } from 'rxjs/operators';
 
-import { HttpService, Injectable } from '@nestjs/common';
+import { HttpService } from '@nestjs/axios';
+import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 
 import { Address, ChainIdEnum, UniswapV3Position } from '@app/common';

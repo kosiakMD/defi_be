c9d0786a430d5b9a18a296e08fce4cdc53a5a1f0
sidlovskyy <sidlovskyy@gmail.com>
2021-12-22 11:08:39 +0200

    Add one time script for Solana tokens load

---
+++ scripts/node_scripts/src/tracked_tokens/solana-scan.ts
@@ -0,0 +1,39 @@
+import axios from 'axios';
+
+import { getTokens } from '../api/solana-scan.api';
+import { log, logError } from '../utils/logger';
+
+export async function execute(): Promise<void> {
+  log('Starting.');
+
+  const { data: { total } } = await getTokens();
+  log('Total number of tokens', total);
+
+  const limit = 50;
+  const solanaChainId = 12;
+
+  for (let index = 0; index < total; index += limit) {
+      try {
+        const { data: { data } } = await getTokens(index, limit);
+
+        for (const { mintAddress, tokenSymbol, priceUst } of data) {
+          if (priceUst) {
+            log(`[${index}] Saving ${tokenSymbol} token for Solana platform`);
+            try {
+              await axios.post('https://account.4849018412.defiyield.app/v1/assets', {
+                address: mintAddress,
+                chain: solanaChainId,
+              });
+              log(`[${index}] Saving ${tokenSymbol} token for Solana platform`);
+            } catch (e) {
+              logError(`[${index}] Token ${tokenSymbol}  saving error`, e);
+            }
+          }
+        }
+      } catch (err) {
+        logError(`Token checking error`, err);
+      }
+  }
+
+  log('Completed.');
+}

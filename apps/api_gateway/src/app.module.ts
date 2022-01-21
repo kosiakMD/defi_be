8f7c13f772f1a5ff4295f37ca67759985633fb7a
kosiakmd <keleborn.mail@gmail.com>
2022-01-21 16:52:51 +0200

    ID-3127, ID-3126 cleaning

--- apps/api_gateway/src/app.module.ts
+++ apps/api_gateway/src/app.module.ts
@@ -11,7 +11,7 @@ import { Logger } from '@app/common';
 import { getWinstonParams } from '@app/common/Logger/logger.config';
 import configuration from '@app/common/config/configuration';
 import { AllExceptionsFilter } from '@app/common/interceptors/AllExceptionsFilter';
-import { RequestIdMiddleware } from '@app/common/middlewares/req-id.middleware';
+import { HeadersMiddleware } from '@app/common/middlewares/headers.middleware';
 
 import { AccountModule } from './account/account.module';
 import { AccountService } from './account/account.service';
@@ -137,7 +137,7 @@ import { VaultsModule } from './vaults/vaults.module';
 })
 export class AppModule implements OnModuleInit, NestModule {
   configure(consumer: MiddlewareConsumer): void {
-    consumer.apply(RequestIdMiddleware, LoggerMiddleware).forRoutes('/');
+    consumer.apply(HeadersMiddleware, LoggerMiddleware).forRoutes('/');
   }
 
   onModuleInit(): void {

971d238fcc72f300be5451c26de5c36f95edf49b
kosiakmd <keleborn.mail@gmail.com>
2022-02-16 10:53:11 +0200

    ID-3391 code improvements

--- libs/common/src/Logger/Logger.service.ts
+++ libs/common/src/Logger/Logger.service.ts
@@ -3,8 +3,6 @@ import { Logger as WinstonLoggerInterface } from 'winston';
 import { LoggerService, LoggerService as NestLoggerService } from '@nestjs/common';
 import { WinstonLogger } from 'nest-winston';
 
-import { HeadersContextMiddleware } from '@app/common/middlewares';
-
 // TODO: TBD?
 // @Injectable({ scope: Scope.TRANSIENT })
 export class Logger extends WinstonLogger implements NestLoggerService {
@@ -13,10 +11,8 @@ export class Logger extends WinstonLogger implements NestLoggerService {
   private static times = new Map();
   private readonly times = new Map();
 
-  constructor(logger: WinstonLoggerInterface, headersContextMiddleware: HeadersContextMiddleware) {
+  constructor(logger: WinstonLoggerInterface) {
     super(logger);
-    console.log('logger', logger);
-    console.log('headersContextMiddleware', headersContextMiddleware);
   }
 
   static time(message: string): number {

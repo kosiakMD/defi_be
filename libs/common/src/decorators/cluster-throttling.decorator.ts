565ca593bcf246081a27d8ce62509ffd2dc59261
kosiakmd <keleborn.mail@gmail.com>
2022-03-17 00:07:34 +0200

    ID-3428 cluster fetch throttling (OpenSea)

--- libs/common/src/decorators/cluster-throttling.decorator.ts
+++ libs/common/src/decorators/cluster-throttling.decorator.ts
@@ -20,42 +20,48 @@ export const ClusterThrottling = function <T = any>(
 
     propertyDescriptor.value = async function (...args: any): Promise<T> {
       const cache: Cache = this.cache;
-      // const logger: Logger = this.logger;
+      // const logge__r: Logger = this.logge__r;
 
       // for debug reason only
       {
         let r = rMap.get(key) || 0;
         r++;
-        console.log('refetch', r);
+        console.log('__refetch', key, r);
         rMap.set(key, r);
       }
 
-      // console.log('cache: ', cache);
+      // console.log('__cache: ', cache);
       // eslint-disable-next-line @typescript-eslint/ban-ts-comment
       // @ts-ignore
-      const redis = cache.store.getClient();
-      console.log('getClient: ', redis);
+      // const redis: Redis = cache.store.getClient();
+      // console.log('__getClient: ', redis);
+
       // const incr = await redis.incr(key);
-      // console.log('incr:', incr);
+      // console.log('__incr:', incr);
       // const current = await redis.call('incr', key);
-      // console.log('current:', current);
-      // console.log('keys', await cache.store.keys());
+      // console.log('__current:', current);
+      // console.log('__keys', await cache.store.keys());
+      // console.log('__', await cache.set(key, 0));
       const count = Number((await cache.get(key)) || 0);
-      console.log('count', count);
+      console.log('__count before', key, count);
+      // delaying in the loop if bus is busy, 200ms by default
       if (count >= limitPerSec) {
+        console.log('__delaying', limitPerSec);
         return new Promise((r) => setTimeout(() => r(method.apply(this, args)), retryInSec));
       } else {
-        // await cache.set(key, count + 1, { ttl: ttlInSec });
+        console.log('__incrementing');
+        await cache.set(key, count + 1, { ttl: ttlInSec });
       }
 
       try {
+        console.log('__trying');
         return await method.apply(this, args);
       } finally {
         const count = Number((await cache.get(key)) || 0);
-        console.log('count2', count);
+        console.log('__count after 2', key, count);
         // const nextCount = count ? count - 1 : 0;
         const nextCount = 1;
-        console.log('nextCount', nextCount);
+        console.log('__nextCount', nextCount);
         rMap.set(key, 0);
         await cache.set(key, nextCount, { ttl: ttlInSec });
       }

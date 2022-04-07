c641b0bdd2a3f538633fa38bc8212acd8b4ee859
vladdn <53942485+vladdn@users.noreply.github.com>
2022-04-07 19:37:19 +0300

    [ID-3690] protocols v3 dtos conversion (#1106)
    * cherry pick: added reserves to staking responses and to user dto
    
    * cherry pick: ce8ff2b05caa4343edd2ae438449c5912ff29dd9

--- apps/integration_service/src/framework/support/interfaces/tokens.common.interface.ts
+++ apps/integration_service/src/framework/support/interfaces/tokens.common.interface.ts
@@ -8,6 +8,10 @@ export interface ERC20Token {
   name: string;
   decimals: number;
   price: number;
+  value?: number;
+  balance?: number;
+  reserve?: number;
+  position?: number;
   underlying: ERC20Token[];
 }
 
--- apps/integration_service/src/modules/integrations/integrations.module.ts
+++ apps/integration_service/src/modules/integrations/integrations.module.ts
@@ -18,6 +18,7 @@ import { ProtocolModule } from '../protocols/protocol.module';
 import { ProjectsInfoEntity } from './entities/projectsInfo.entity';
 import { FeaturesService } from './features.service';
 import { IntegrationsService } from './integrations.service';
+import { IntegrationsServiceV3Decorator } from './integrations.service.v3.decorator';
 
 // TODO to add a new Protocol just add it here and at ProtocolService constructor
 
@@ -48,6 +49,7 @@ import { IntegrationsService } from './integrations.service';
     MulticallAggregator,
     Web3ProviderService,
     Web3SolanaProviderService,
+    IntegrationsServiceV3Decorator,
   ],
   controllers: [IntegrationsController, IntegrationsControllerV2, IntegrationsControllerV3],
 })

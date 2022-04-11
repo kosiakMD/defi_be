eda92b7d3f17cb67f1797498a6311779ef462966
IgorByk <97164174+IgorByk@users.noreply.github.com>
2022-04-11 09:28:49 +0300

    Id 3720 lambdas scheduler and cleanup (#1103)
    * Scheduler and Price_cleanup  Jenkinsfiles and terraform scenario

--- terraform_scenario/.data/parameters_from_env/parameters_from_env.sh
+++ terraform_scenario/.data/parameters_from_env/parameters_from_env.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
+#environment="STAGING"
 environment="DEV"
+#env_region="us-west-2"
+env_region="eu-central-1"
 IFS="="
 while read -r name value
 do
@@ -8,13 +11,13 @@ do
   if ! [[ $name = \#* ]] && ! [[ "-$names" == "-" ]] ;
    then
   echo "Content of $name is $values"
-#  aws ssm delete-parameter --name "/$environment/$names"
-#  aws ssm put-parameter --name "/$environment/$names" --type "SecureString"  --value "$values"
+  aws ssm delete-parameter --region $env_region --name "/$environment/$names"
+  aws ssm put-parameter --region $env_region --name "/$environment/$names" --type "SecureString"  --value "$values"
   echo "data \"aws_ssm_parameter\" \"$environment_$names\" {" >> 01-datas.tf
-  echo "  name = \"/$environment/$names\"" >> 01-datas.tf
+  echo "  name = upper(\"/\${var.environment}/$names\"" >> 01-datas.tf
   echo "}" >> 01-datas.tf
   echo >> 01-datas.tf
   echo "    \"$names\" = data.aws_ssm_parameter.$environment_$names.value," >> listformain.txt
  fi
 
-done < ../.env
+done < .parameters

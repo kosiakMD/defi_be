af1edd891f56a88220742ba8cfb1988ab7cf11e3
MykolaBorodyn <mykolab@defiyield.app>
2022-04-11 10:10:19 +0300

    Add name to address suggestions results

--- apps/api_gateway/src/search/dto/address-suggestion.dto.ts
+++ apps/api_gateway/src/search/dto/address-suggestion.dto.ts
@@ -1,9 +1,13 @@
 import { ApiProperty } from '@nestjs/swagger';
 
 export class AddressSuggestionDto {
-  constructor(address: string) {
+  constructor(address: string, name?: string) {
     this.address = address;
+    this.name = name;
   }
   @ApiProperty({ type: String, example: '0xcd2E72aEBe2A203b84f46DEEC948E6465dB51c75' })
   address: string;
+
+  @ApiProperty({ type: String, example: 'beka.eth' })
+  name: string;
 }

a4caf358d21147f1dbd940d4f69afbf254c9d683
Roman Hreits <46167139+RomanHreits@users.noreply.github.com>
2022-04-13 13:45:28 +0100

    hotfix (#1147)

--- jobs/lambda_vaults/src/jobs/jobs.module.ts
+++ jobs/lambda_vaults/src/jobs/jobs.module.ts
@@ -8,7 +8,6 @@ import { MulticallAggregator } from '@app/common/web3provider/multicall.aggregat
 
 import { MicroservicesModule } from '../microservices/microservices.module';
 import { StoreModule } from '../store/store.module';
-import { OsmosisPools } from './Osmosis/Osmosis.pools';
 import { AnchorLp } from './anchor/anchor.lp';
 import { AnchorStaking } from './anchor/anchor.staking';
 import { AstroportLp } from './astroport/astroport.lp';

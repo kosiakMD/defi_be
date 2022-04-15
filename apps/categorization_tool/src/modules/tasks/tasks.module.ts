a44bebba1bdf3f2f920d9397a49016fbf585f089
Pavlo Karpovets <97891236+pavlokdefi@users.noreply.github.com>
2022-04-15 12:29:21 +0300

    [ID-3854] Added redis auth and simple status endpoint. (#1160)

--- apps/categorization_tool/src/modules/tasks/tasks.module.ts
+++ apps/categorization_tool/src/modules/tasks/tasks.module.ts
@@ -17,6 +17,7 @@ import { TasksService } from './tasks.service';
         redis: {
           host: configService.get<string>('REDIS_HOST'),
           port: +configService.get<number>('REDIS_PORT'),
+          password: configService.get<string>('REDIS_AUTH'),
         },
       }),
       inject: [ConfigService],

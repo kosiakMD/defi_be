2da79dc1e0e45696748789ecff5f72b0265f15b7
Stelyan Georgiev <103415561+stelyang@users.noreply.github.com>
2022-04-19 16:18:10 +0300

    Stelyang/feature/id 3833 fix lido request validate addresses (#1175)
    * [ID-3833]: added basic validators
    
    * [ID-3833]: added tests
    
    * [ID-3833]: refactored to use address filtering in two single points
    
    * quick fix sol_url problem
    
    * [ID-3833]: added validation for all EVM and ronin
    
    * remove package lock
    
    * revert SOL_URL, due to mistake

--- apps/integration_service/src/modules/protocols/protocols/traderjoe/trader-joe.protocol.ts
+++ apps/integration_service/src/modules/protocols/protocols/traderjoe/trader-joe.protocol.ts
@@ -3,7 +3,6 @@ import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
 
 import { Address, ChainDto, FeatureEnum, Logger } from '@app/common';
 import { ChainAbbrEnum, ProjectEnum, TraderjoeProtocolEnum } from '@app/common/enum';
-import { keepETHAddresses } from '@app/common/utils';
 
 import { BaseData } from '../../../../common/interfaces/transactions.interfaces';
 
@@ -46,7 +45,6 @@ export default class TraderJoeProtocol extends BasicProtocol {
     addresses: Address[],
     chain: ChainDto,
   ): Promise<[BaseData[], string[]]> {
-    addresses = keepETHAddresses(addresses);
     const chainFeatures = await Promise.allSettled(
       this.features[chain.abbr].map((f) => {
         return this.getFeatureData(addresses, chain, f);

25a3ba7f7865851433c5d3dc967ffd2872c116c7
Andrii Tymchenko <16686943+x-ror@users.noreply.github.com>
2022-04-20 10:01:18 +0300

    [ID-3870,3874,3875,3909,3758] framework fixes (#1180)
    * balancer fix
    
    * fixed balancer staking
    
    * normalize reserve
    
    * updated pool length

--- apps/integration_service/src/framework/support/EVM/protocols/Yield/MasterChef.ts
+++ apps/integration_service/src/framework/support/EVM/protocols/Yield/MasterChef.ts
@@ -312,7 +312,7 @@ export class MasterChef
     if (pool.supplied[0].token.underlying?.length === 2) {
       const poolShare = balance / pool.supplied[0].totalSupplied;
       pool.supplied[0].token.underlying.forEach((u) => {
-        u.balance = u.reserve * poolShare;
+        u.balance = normalizeDecimals(u.reserve.toString(), u.decimals) * poolShare;
         u.value = u.balance * u.price;
       });
     }

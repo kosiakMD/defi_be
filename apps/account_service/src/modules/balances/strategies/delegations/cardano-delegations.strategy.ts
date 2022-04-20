646a4486c5923733de77e886e6b794ecbecf58cf
kosiakmd <keleborn.mail@gmail.com>
2022-04-21 00:58:08 +0300

    ID-3264 General Optimization of the dashboard loading
     - optimise Cardano Strategy

--- apps/account_service/src/modules/balances/strategies/delegations/cardano-delegations.strategy.ts
+++ apps/account_service/src/modules/balances/strategies/delegations/cardano-delegations.strategy.ts
@@ -1,11 +1,14 @@
+import { lastValueFrom } from 'rxjs';
 import { Repository } from 'typeorm';
 
 import { HttpService } from '@nestjs/axios';
 import { Inject, Injectable, OnModuleInit } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { InjectRepository } from '@nestjs/typeorm';
 import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
 
-import { Logger } from '@app/common';
+import { ChainIdEnum, Logger } from '@app/common';
+import { CARDANO_COIN_ADDRESS } from '@app/common/constant';
 import { isCardanoAddress, normalizeDecimals } from '@app/common/utils';
 
 import { PriceService } from '../../../../common/providers/microservices/price/price.service';
@@ -16,7 +19,16 @@ import { DelegationsStrategy } from './index';
 @Injectable()
 export class CardanoDelegationsStrategy extends DelegationsStrategy implements OnModuleInit {
   private asset: AssetsEntity;
+
+  protected url = 'https://cardano-mainnet.blockfrost.io/api/v0';
+
+  private headers = {
+    // eslint-disable-next-line camelcase
+    project_id: this.configService.get<string>('CARDANO_BLOCKFROST_API_KEY'),
+  };
+
   constructor(
+    private configService: ConfigService,
     private readonly http: HttpService,
     private readonly priceService: PriceService,
     @Inject(WINSTON_MODULE_NEST_PROVIDER) protected readonly logger: Logger,
@@ -28,63 +40,56 @@ export class CardanoDelegationsStrategy extends DelegationsStrategy implements O
 
   async onModuleInit(): Promise<void> {
     this.asset = await this.assetsRepository.findOne({
-      address: 'addr100000000000000000000000000000000000000000000',
-      chain: 22,
+      address: CARDANO_COIN_ADDRESS,
+      chain: ChainIdEnum.cardano,
     });
   }
 
-  url = 'https://cardano-mainnet.blockfrost.io/api/v0';
+  private async getFeatures(address): Promise<any[]> {
+    const getConfig = { headers: this.headers };
 
-  private headers = {
-    // eslint-disable-next-line camelcase
-    project_id: 'mainnetDGLOdhwzeWRwDgIrlO22kRh3BJwPA2t0',
-  };
+    const { data: addressResponse } = await lastValueFrom(
+      this.http.get(`${this.url}/addresses/${address}`, getConfig),
+    );
+    const { data: stakeData } = await lastValueFrom(
+      this.http.get(`${this.url}/accounts/${addressResponse.stake_address}`, getConfig),
+    );
+    const { data: poolData } = await lastValueFrom(
+      this.http.get(`${this.url}/pools/${stakeData.pool_id}/metadata`, getConfig),
+    );
+    return [addressResponse, stakeData, poolData];
+  }
 
   public async getDelegatedAssets(address) {
     if (!isCardanoAddress(address)) return [];
-    const { prices } = await this.priceService.fetchTokenPrices(
-      [this.asset.address],
-      this.asset.chain,
-    );
 
-    try {
-      const { data: addressResponse } = await this.http
-        .get(`${this.url}/addresses/${address}`, { headers: this.headers })
-        .toPromise();
+    const [{ prices }, [, stakeData, poolData]] = await Promise.all([
+      this.priceService.fetchTokenPrices([this.asset.address], this.asset.chain),
+      this.getFeatures(address),
+    ]);
 
-      const { data: stakeData } = await this.http
-        .get(`${this.url}/accounts/${addressResponse.stake_address}`, { headers: this.headers })
-        .toPromise();
+    const balanceAmount = normalizeDecimals(stakeData.controlled_amount, this.asset.decimals);
+    const claimableRewardsAmount = normalizeDecimals(stakeData.rewards_sum, this.asset.decimals);
+    const price = prices[this.asset.address];
 
-      const { data: poolData } = await this.http
-        .get(`${this.url}/pools/${stakeData.pool_id}/metadata`, { headers: this.headers })
-        .toPromise();
-
-      return [
-        {
-          address,
-          asset: this.asset,
-          validator: {
-            address: poolData.pool_id,
-            name: poolData.name,
-            website: poolData.homepage,
-          },
-          balance: {
-            amount: normalizeDecimals(stakeData.controlled_amount, this.asset.decimals),
-            amountUsd:
-              normalizeDecimals(stakeData.controlled_amount, this.asset.decimals) *
-              prices[this.asset.address],
-          },
-          claimableRewards: {
-            amount: normalizeDecimals(stakeData.rewards_sum, this.asset.decimals),
-            amountUsd:
-              normalizeDecimals(stakeData.rewards_sum, this.asset.decimals) *
-              prices[this.asset.address],
-          },
+    return [
+      {
+        address,
+        asset: this.asset,
+        validator: {
+          address: poolData.pool_id,
+          name: poolData.name,
+          website: poolData.homepage,
         },
-      ];
-    } catch (err) {
-      this.logger.error(err);
-    }
+        balance: {
+          amount: balanceAmount,
+          amountUsd: balanceAmount * price,
+        },
+        claimableRewards: {
+          amount: claimableRewardsAmount,
+          amountUsd: claimableRewardsAmount * price,
+        },
+      },
+    ];
   }
 }

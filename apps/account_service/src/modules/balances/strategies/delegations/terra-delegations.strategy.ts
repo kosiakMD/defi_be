25bb1598de13d4d8dbc870f7e8b8cff73567d427
Anton Kosiak <kosiakmd@gmail.com>
2022-04-28 16:08:49 +0300

    ID-3264 general optimization of the dashboard loading - Optimise Terra Strategy (#1194)
     - optimise Terra Strategy

--- apps/account_service/src/modules/balances/strategies/delegations/terra-delegations.strategy.ts
+++ apps/account_service/src/modules/balances/strategies/delegations/terra-delegations.strategy.ts
@@ -1,3 +1,5 @@
+import { lastValueFrom } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { Repository } from 'typeorm';
 
 import { HttpService } from '@nestjs/axios';
@@ -5,18 +7,22 @@ import { Inject, Injectable, OnModuleInit } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
 
-import { Logger } from '@app/common';
+import { ChainIdEnum, Logger } from '@app/common';
 import { handlePromiseAllSettled } from '@app/common/helpers/promises';
 import { isTerraAddress, normalizeDecimals } from '@app/common/utils';
 
 import { PriceService } from '../../../../common/providers/microservices/price/price.service';
 
+import { StaderAddresses } from '../../../../../../integration_service/src/modules/protocols/protocols/stader/stader.addresses';
 import { AssetsEntity } from '../../../assets/entities/assets.entity';
 import { DelegationsStrategy } from './index';
 
 @Injectable()
 export class TerraDelegationsStrategy extends DelegationsStrategy implements OnModuleInit {
   private asset: AssetsEntity;
+
+  // TODO: Move to .env file
+  protected url = 'https://lcd.terra.dev/cosmos/staking/v1beta1/validators';
 
   constructor(
     private readonly http: HttpService,
@@ -30,58 +36,67 @@ export class TerraDelegationsStrategy extends DelegationsStrategy implements OnM
 
   async onModuleInit(): Promise<void> {
     this.asset = await this.assetsRepository.findOne({
-      address: 'uluna',
+      address: StaderAddresses.luna,
       // TODO: This chain id cannot be hardcoded
-      chain: 19,
+      chain: ChainIdEnum.terra,
     });
   }
 
-  // TODO: Move to .env file?
-  url = 'https://lcd.terra.dev/cosmos/staking/v1beta1/validators';
+  private async getData(address: string): Promise<any[]> {
+    const promises = [];
+
+    const validators = await this.getValidators();
+
+    for (const validator of validators) {
+      promises.push(
+        lastValueFrom(
+          this.http.get(`${this.url}/${validator.operator_address}/delegations/${address}`),
+        ),
+      );
+    }
+
+    const data = handlePromiseAllSettled(await Promise.allSettled(promises))[0];
+
+    return [validators, data];
+  }
 
   public async getDelegatedAssets(address) {
     if (!isTerraAddress(address)) return [];
     const result = [];
-    const promises = [];
 
     try {
-      const validators = await this.getValidators();
-      const { prices } = await this.priceService.fetchTokenPrices(
-        [this.asset.address],
-        this.asset.chain,
+      const [{ prices }, [validators, data]] = await Promise.all([
+        this.priceService.fetchTokenPrices([this.asset.address], this.asset.chain),
+        this.getData(address),
+      ]);
+
+      const validatorsMap: Map<string, any> = new Map(
+        validators.map((v) => [v.operator_address, v]),
       );
 
-      for (const validator of validators) {
-        promises.push(
-          this.http
-            .get(`${this.url}/${validator.operator_address}/delegations/${address}`)
-            .toPromise(),
-        );
-      }
-
-      const results = await Promise.allSettled(promises);
-      const [data] = handlePromiseAllSettled(results);
-
       data.forEach((r) => {
-        const validator = validators.find(
-          (v) => v.operator_address === r.data.delegation_response.delegation.validator_address,
+        const validator = validatorsMap.get(
+          r.data.delegation_response.delegation.validator_address,
         );
+        const balanceAmount = normalizeDecimals(
+          r.data.delegation_response.balance.amount,
+          this.asset.decimals,
+        );
+        const price = prices[this.asset.address];
+
         result.push({
           address,
-          asset: { ...this.asset, price: prices[this.asset.address] },
+          // TODO: add correct AssetDTO extended from AssetEntity
+          //  with omitting redundant methods and properties
+          asset: { ...this.asset, price },
           validator: {
             address: validator.operator_address,
             name: validator.description.moniker,
             website: validator.description.website,
           },
           balance: {
-            amount: normalizeDecimals(
-              r.data.delegation_response.balance.amount,
-              this.asset.decimals,
-            ),
-            amountUsd:
-              normalizeDecimals(r.data.delegation_response.balance.amount, this.asset.decimals) *
-              prices[this.asset.address],
+            amount: balanceAmount,
+            amountUsd: balanceAmount * price,
           },
         });
       });
@@ -94,9 +109,9 @@ export class TerraDelegationsStrategy extends DelegationsStrategy implements OnM
   }
 
   private async getValidators() {
-    const { data: validatorsData } = await this.http
-      .get(`${this.url}?pagination.limit=999`)
-      .toPromise();
+    const validatorsData = await lastValueFrom(
+      this.http.get(`${this.url}?pagination.limit=999`).pipe(map(({ data }) => data)),
+    );
 
     return validatorsData.validators.filter((v) => !v.jailed);
   }

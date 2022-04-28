aa86a9cd957c330261608d29dc9f7491b8381b7a
kosiakmd <keleborn.mail@gmail.com>
2022-04-28 16:01:48 +0300

    ID-3264 general optimization of the dashboard loading
    - Optimise Terra Strategy

--- apps/account_service/src/modules/balances/strategies/delegations/terra-delegations.strategy.ts
+++ apps/account_service/src/modules/balances/strategies/delegations/terra-delegations.strategy.ts
@@ -4,7 +4,6 @@ import { Repository } from 'typeorm';
 
 import { HttpService } from '@nestjs/axios';
 import { Inject, Injectable, OnModuleInit } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
 import { InjectRepository } from '@nestjs/typeorm';
 import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
 

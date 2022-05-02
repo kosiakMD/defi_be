721bd805c7f273673863e307bfbfa372787c999a
Andrii Tymchenko <16686943+x-ror@users.noreply.github.com>
2022-05-02 19:14:10 +0300

    made handle batches method protected and overridden (#1235)

--- apps/integration_service/src/framework/support/EVM/EVMCore.ts
+++ apps/integration_service/src/framework/support/EVM/EVMCore.ts
@@ -1,3 +1,4 @@
+import { CurrentPricesPayload } from 'apps/integration_service/src/common/dto/price.response.dto';
 import BigNumber from 'bignumber.js';
 import { Cache } from 'cache-manager';
 
@@ -56,71 +57,11 @@ export abstract class EVMCore<
           this.meta.chain,
         );
 
-        /**
-         * TODO: This is a hack to get the price of missing tokens and calculate the price of LP tokens.
-         * It makes the assumption that every token which has an underlying token is a Uniswap V2 token
-         * which is likely the case with MasterChef (but not always, see beets.fi, a balancer fork)
-         * however this class aims to be be somewhat protocol agnostic, so ideally these prices/calculations
-         * come from the asset service so we aren't required to categorize tokens and adapt different pricing
-         * strategies here. For a direct token price lookup via the API its best to have an averaged price
-         * like we do, but taking into account these are specific LP's at specific DEXes, I think just using
-         * the raw price is acceptable here if needed (a SpookySwap LP is only available at SpookySwap so no
-         * advantage in averaging that price across dexes)
-         */
-
-        try {
-          const calls = new Map();
-          tokens.forEach((token: any) => {
-            if (!token.underlyingAssets?.length) return;
-            const contract = new UniswapV2Pair(token.address);
-            calls.set(`${token.address}.totalSupply()`, contract.totalSupply());
-            calls.set(`${token.address}.getReserves()`, contract.getReserves());
-            calls.set(`${token.address}.token0()`, contract.token0());
-            calls.set(`${token.address}.token1()`, contract.token1());
-          });
-          const results = await this.multicall.handleInBatches(calls, this.meta.chain);
-          tokens.forEach((token: any) => {
-            if (token.underlyingAssets?.length !== 2) return;
-            const totalSupply = results.get(`${token.address}.totalSupply()`).output.data;
-            const token0Address = results
-              .get(`${token.address}.token0()`)
-              .output.data.toLowerCase();
-            const token1Address = results
-              .get(`${token.address}.token1()`)
-              .output.data.toLowerCase();
-            const { _reserve0, _reserve1 } = results.get(`${token.address}.getReserves()`).output
-              .data;
-            if (!Number(prices[token0Address]) && !Number(prices[token1Address])) return;
-
-            // calculate/fill in missing base token prices based on current LP reserves
-            if (!prices[token0Address]) {
-              prices[token0Address] = _reserve1.times(prices[token1Address]).div(_reserve0);
-            }
-            if (!prices[token1Address]) {
-              prices[token1Address] = _reserve1.times(prices[token0Address]).div(_reserve1);
-            }
-
-            // calculate/fill the LP token price into the price array
-            const tvl0 = _reserve0.times(prices[token0Address]);
-            const tvl1 = _reserve1.times(prices[token1Address]);
-            prices[token.address] = new BigNumber(tvl0.plus(tvl1).toString()) //
-              .div(totalSupply)
-              .toNumber();
-            token.underlyingAssets.forEach((u) => {
-              u.reserve = normalizeDecimals(
-                (u.reserve = u.positionInPool === 0 ? _reserve0 : _reserve1).toString(),
-                u.decimals,
-              );
-            });
-          });
-        } catch (err) {
-          // TODO: delete this block, Prices should come from asset service, not calculated here
-          this.logger.error(err.message, err.stack, 'EVMCore');
-        }
+        const tokenUpdated = await this.updateTokenData(tokens, prices);
 
         // need :any as Asset doesn't have underlying tokens
         // return entries, but not a map so we can cache (serialize/deserialize) easily
-        return tokens.map((token: any) => {
+        return tokenUpdated.map((token: any) => {
           return [
             token.address,
             {
@@ -201,5 +142,68 @@ export abstract class EVMCore<
     });
 
     return Array.from(tokens);
+  }
+
+  /**
+   * TODO: This is a hack to get the price of missing tokens and calculate the price of LP tokens.
+   * It makes the assumption that every token which has an underlying token is a Uniswap V2 token
+   * which is likely the case with MasterChef (but not always, see beets.fi, a balancer fork)
+   * however this class aims to be be somewhat protocol agnostic, so ideally these prices/calculations
+   * come from the asset service so we aren't required to categorize tokens and adapt different pricing
+   * strategies here. For a direct token price lookup via the API its best to have an averaged price
+   * like we do, but taking into account these are specific LP's at specific DEXes, I think just using
+   * the raw price is acceptable here if needed (a SpookySwap LP is only available at SpookySwap so no
+   * advantage in averaging that price across dexes)
+   */
+  protected async updateTokenData(
+    tokens: any[],
+    prices: CurrentPricesPayload,
+  ): Promise<ERC20Token[]> {
+    try {
+      const calls = new Map();
+      tokens.forEach((token: any) => {
+        if (!token.underlyingAssets?.length) return;
+        const contract = new UniswapV2Pair(token.address);
+        calls.set(`${token.address}.totalSupply()`, contract.totalSupply());
+        calls.set(`${token.address}.getReserves()`, contract.getReserves());
+        calls.set(`${token.address}.token0()`, contract.token0());
+        calls.set(`${token.address}.token1()`, contract.token1());
+      });
+      const results = await this.multicall.handleInBatches(calls, this.meta.chain);
+      tokens.forEach((token: any) => {
+        if (token.underlyingAssets?.length !== 2) return;
+        const totalSupply = results.get(`${token.address}.totalSupply()`).output.data;
+        const token0Address = results.get(`${token.address}.token0()`).output.data.toLowerCase();
+        const token1Address = results.get(`${token.address}.token1()`).output.data.toLowerCase();
+        const { _reserve0, _reserve1 } = results.get(`${token.address}.getReserves()`).output.data;
+        if (!Number(prices[token0Address]) && !Number(prices[token1Address])) return;
+
+        // calculate/fill in missing base token prices based on current LP reserves
+        if (!prices[token0Address]) {
+          prices[token0Address] = _reserve1.times(prices[token1Address]).div(_reserve0);
+        }
+        if (!prices[token1Address]) {
+          prices[token1Address] = _reserve1.times(prices[token0Address]).div(_reserve1);
+        }
+
+        // calculate/fill the LP token price into the price array
+        const tvl0 = _reserve0.times(prices[token0Address]);
+        const tvl1 = _reserve1.times(prices[token1Address]);
+        prices[token.address] = new BigNumber(tvl0.plus(tvl1).toString()) //
+          .div(totalSupply)
+          .toNumber();
+        token.underlyingAssets.forEach((u) => {
+          u.reserve = normalizeDecimals(
+            (u.reserve = u.positionInPool === 0 ? _reserve0 : _reserve1).toString(),
+            u.decimals,
+          );
+        });
+      });
+      return tokens;
+    } catch (err) {
+      // TODO: delete this block, Prices should come from asset service, not calculated here
+      this.logger.error(err.message, err.stack, 'EVMCore');
+      return tokens;
+    }
   }
 }

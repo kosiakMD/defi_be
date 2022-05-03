3891a5cd332099905c3678031ba7ec35d7386669
Pavlo Karpovets <97891236+pavlokdefi@users.noreply.github.com>
2022-05-03 10:30:56 +0300

    [ID-4033] Added cron job to start fetching once per day at 8 pm. (#1245)

--- apps/categorization_tool/src/modules/tasks/tasks.service.ts
+++ apps/categorization_tool/src/modules/tasks/tasks.service.ts
@@ -2,9 +2,11 @@ import { Queue } from 'bull';
 
 import { InjectQueue } from '@nestjs/bull';
 import { Inject, Injectable, Logger } from '@nestjs/common';
+import { Cron, CronExpression } from '@nestjs/schedule';
 import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
 
 import { REDIS_TASK_QUEUE, COMMON_TASK } from '../../common/constants';
+import { Command } from '../../common/enum/service.enum';
 
 @Injectable()
 export class TasksService {
@@ -12,6 +14,13 @@ export class TasksService {
     @Inject(WINSTON_MODULE_NEST_PROVIDER) protected readonly logger: Logger,
     @InjectQueue(REDIS_TASK_QUEUE) private readonly queue: Queue,
   ) {}
+
+  @Cron(CronExpression.EVERY_DAY_AT_8PM)
+  async cronTask() {
+    const task = { command: Command.start_fetching };
+    this.logger.log(`cronTask: ${task.command}`);
+    await this.queueTask(task);
+  }
 
   public async queueTask(data): Promise<void> {
     try {

d9fbf9ecdcd4cccc4f0f9144c169578a16d1cf80
Anton Kosiak <kosiakmd@gmail.com>
2022-05-03 14:57:04 +0300

    ID-4096 Disable Sentry Axios Error 500x (#1250)
    * ID-4096 Disable Sentry Axios Error 500x
    
    * ID-4096 Disable Sentry Axios Error 500x

--- libs/common/src/bootstrap/initSentry.ts
+++ libs/common/src/bootstrap/initSentry.ts
@@ -1,17 +1,72 @@
+import { RewriteFrames } from '@sentry/integrations';
 import * as Sentry from '@sentry/node';
 
-// Importing @sentry/tracing patches the global hub for tracing to work.
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
+// This allows TypeScript to detect our global value
+declare global {
+  // eslint-disable-next-line @typescript-eslint/no-namespace
+  export namespace NodeJS {
+    interface Global {
+      __rootdir__: string;
+    }
+  }
+}
 
+// eslint-disable-next-line no-underscore-dangle
+global.__rootdir__ = __dirname || process.cwd();
+
+// This allows TypeScript to detect our global value
+declare global {
+  // eslint-disable-next-line @typescript-eslint/no-namespace
+  export namespace NodeJS {
+    interface Global {
+      __rootdir__: string;
+    }
+  }
+}
+
+// eslint-disable-next-line no-underscore-dangle
+global.__rootdir__ = __dirname || process.cwd();
+
+const TEST_SENTRY = false;
 // arg sentryDSN for test reason only
-export const initSentry = function (sentryDSN?: string): void {
+export const initSentry = function (sentryDSN = process.env.SENTRY_DSN as string): void {
+  if (!TEST_SENTRY && !sentryDSN) return;
   Sentry.init({
-    dsn: sentryDSN || process.env.SENTRY_DSN, // will be read as config module use dotenv for sync .env reading
+    dsn: sentryDSN,
     environment: process.env.NODE_ENV,
     serverName: process.env.SERVICE_NAME,
     attachStacktrace: true, // TODO: disable in prod
     normalizeDepth: 10, // TODO: reduce in prod
     debug: process.env.LOG_LEVEL === 'debug',
+    integrations: [
+      new RewriteFrames({
+        // eslint-disable-next-line no-underscore-dangle
+        root: global.__rootdir__,
+      }),
+    ],
+    beforeSend: function (event: Sentry.Event, hint: Sentry.EventHint) {
+      const exception: any = hint.originalException;
+
+      if (exception.isAxiosError) {
+        // TODO turn of axios HTTP Error = 500 for a while
+        const excluded = [500, 502];
+        const code =
+          exception?.request?.res?.statusCode ||
+          exception?.response?.status ||
+          exception?.response?.data?.status_code;
+        if (excluded.includes(code)) {
+          return null;
+        } else {
+          event.fingerprint = [
+            '{{ default }}',
+            String(exception.functionName),
+            String(exception.errorCode),
+          ];
+        }
+      }
+
+      return event;
+    },
     // We recommend adjusting this value in production, or using tracesSampler
     // for finer control
     tracesSampleRate: 1.0,

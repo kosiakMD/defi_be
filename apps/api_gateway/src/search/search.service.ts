fef30415b031c9ed58f8c4748048dce915478751
Mykola Borodyn <97102547+NickDeFi@users.noreply.github.com>
2022-05-10 15:49:36 +0300

    NickDeFi/bugfix/Global search add resolved address to results(fix resolved address entry data) (#1288)
    * Id-3553
    
    * modified chainsService
    
    * fixed scan services
    
    * fixes
    
    * refactor
    
    * removed unused values
    
    * refactor
    
    * fixed eth chain id. Moved private methods to the botton
    
    * resolve comments
    
    * web3 provider
    
    * ID-3553
    
    * fixes
    
    * fixed getBalancesStrategiesPerChain
    
    * removed comments
    
    * added new env validation
    
    * web3 provider fix on solana url
    
    * web3 provider. fixed bugs on RPC urls and eth web3
    
    * Update yarn.lock
    
    * Fix lint
    
    * Add name to address suggestions results
    
    * Disable database sync
    
    * Add resolved address to search results
    
    * Fix resolved address entry data
    
    Co-authored-by: MaximBazadji <joe.white1992_02@gmail.com>

--- apps/api_gateway/src/search/search.service.ts
+++ apps/api_gateway/src/search/search.service.ts
@@ -59,7 +59,7 @@ export class SearchService extends BaseService {
       if (address) {
         this.logger.debug(`Resolved address ${address}`);
         const searchResult = await this.getSearchEntries({ address, text, limit });
-        searchResult.entries.push(this.getAddressSearchEntry(text));
+        searchResult.entries.push(this.getAddressSearchEntry(address));
         return addressSearchResultParser(address, searchResult);
       }
     } catch (error) {

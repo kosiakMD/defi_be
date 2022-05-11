05c08b35a09941773fa5421726a9ebbc56d27343
ptretiak <103569408+ptretiak@users.noreply.github.com>
2022-05-12 00:26:44 +0100

    [ID-4128] ptretiak/bugfix/coingecko pro prices (#1310)
    * added api key to coingecko

--- jobs/lambda_external_prices/src/config.ts
+++ jobs/lambda_external_prices/src/config.ts
@@ -1,6 +1,7 @@
 export const priceServiceUrl = process.env.PRICE_SERVICE_URL;
 export const tokenServiceUrl = process.env.TOKEN_SERVICE_URL;
 export const coingeckoApiUrl = process.env.COINGECKO_API_URL;
+export const coingeckoApiKey = process.env.COINGECKO_API_KEY;
 export const priceUpdateLimitInHour = process.env.PRICE_UPDATE_LIMIT_IN_HOURS;
 export const debankApiUrl = 'https://openapi.debank.com';
 export const solPublicAssetsApi = 'https://public-api.solscan.io';

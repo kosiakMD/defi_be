1f876b6649cb27431afdd9a1ff959473766a9c5d
Pavlo Karpovets <97891236+pavlokdefi@users.noreply.github.com>
2022-05-11 19:22:02 +0300

    [3944] Improved logic of comparing contracts with templates. Increased resources limits. Disabled cron running for testing purposes. (#1305)

--- apps/categorization_tool/src/modules/tasks/tasks.service.ts
+++ apps/categorization_tool/src/modules/tasks/tasks.service.ts
@@ -2,7 +2,7 @@ import { JobStatusClean, Queue } from 'bull';
 
 import { InjectQueue } from '@nestjs/bull';
 import { Inject, Injectable, Logger } from '@nestjs/common';
-import { Cron, CronExpression } from '@nestjs/schedule';
+// import { Cron, CronExpression } from '@nestjs/schedule';
 import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
 
 import { REDIS_TASK_QUEUE, COMMON_TASK } from '../../common/constants';
@@ -15,7 +15,7 @@ export class TasksService {
     @InjectQueue(REDIS_TASK_QUEUE) private readonly queue: Queue,
   ) {}
 
-  @Cron(CronExpression.EVERY_DAY_AT_8PM)
+  // @Cron(CronExpression.EVERY_DAY_AT_8PM) //uncomment to enable daily running
   async cronTask() {
     const task = { command: CommandUnparameterized.start_fetching };
     this.logger.log(`cronTask: ${task.command}`);

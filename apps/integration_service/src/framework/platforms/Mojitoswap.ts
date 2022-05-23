12f80eec368eb43cc7aea4ba0241be9097ac7e30
Nikoloz Pestvenidze <62833277+NikaTheEngineer@users.noreply.github.com>
2022-05-23 11:52:19 +0400

    [ID-4134] integrate mojitoswap v2 (#1390)
    * nikoloz-pestvenidze/feature/ID-4134 register new protocol and add new abi provider
    
    * nikoloz-pestvenidze/feature/ID-4134 implement BlockScout service
    
    * nikoloz-pestvenidze/feature/ID-4134 remove warnning
    
    * nikoloz-pestvenidze/feature/ID-4134 add LocalFile service to get ABI when other services fail
    
    * nikoloz-pestvenidze/feature/ID-4134 integrate wine pool auto
    
    * nikoloz-pestvenidze/feature/ID-4134 add kcc blockscout url
    
    * nikoloz-pestvenidze/feature/ID-4134 stylistic changes

--- apps/integration_service/src/framework/platforms/Mojitoswap.ts
+++ apps/integration_service/src/framework/platforms/Mojitoswap.ts
@@ -8,6 +8,8 @@ import {
   IUniswapVaultMeta,
   UniswapV2Liquidity,
 } from '../support/EVM/protocols/Liquidity/UniswapV2Liquidity';
+import { MasterChef } from '../support/EVM/protocols/Yield/MasterChef';
+import { MojitoVault } from '../support/EVM/protocols/Yield/MojitoVault';
 import { RootPlatform } from '../support/RootPlatform';
 import { FeatureEnum } from '../support/enums';
 
@@ -34,6 +36,20 @@ export class Mojitoswap extends RootPlatform {
       name: 'Liquidity - Mojitoswap',
       feature: FeatureEnum.pools,
       ammSubgraphUrl: 'https://thegraph.kcc.network/subgraphs/name/mojito/swap',
+    });
+
+    await this.registerProtocol(MasterChef, {
+      chain: ChainIdEnum.kcc,
+      name: 'Wine Pool Manual - Mojitoswap',
+      feature: FeatureEnum.staking,
+      address: '0xfdfce767add9dcf032cbd0de35f0e57b04495324',
+    });
+
+    await this.registerProtocol(MojitoVault, {
+      chain: ChainIdEnum.kcc,
+      name: 'Wine Pool Auto - Mojitoswap',
+      feature: FeatureEnum.staking,
+      address: '0x3db85383ab912c9d9c003bd2ec09107e0f37b8a0',
     });
   }
 }
--- apps/integration_service/src/framework/support/EVM/AbiModule/BlockScout.service.ts
+++ apps/integration_service/src/framework/support/EVM/AbiModule/BlockScout.service.ts
@@ -1,17 +1,88 @@
+import { firstValueFrom } from 'rxjs';
 import type { AbiItem } from 'web3-utils';
 
 import { HttpService } from '@nestjs/axios';
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
 
-import type { Address, ChainId } from '@app/common';
+import type { Address, ChainId, Logger } from '@app/common';
+import { ChainIdEnum } from '@app/common';
+
+interface IBlockScoutResponse {
+  data: {
+    data?: {
+      address: {
+        smartContract: {
+          abi: string;
+        };
+      };
+    };
+    errors?: {
+      locations?: {
+        column: number;
+        line: number;
+      }[];
+      message: string;
+    }[];
+  };
+}
 
 @Injectable()
 export class BlockScout {
-  constructor(protected httpService: HttpService, protected config: ConfigService) {}
+  constructor(
+    protected httpService: HttpService,
+    protected config: ConfigService,
+    @Inject(WINSTON_MODULE_NEST_PROVIDER) protected logger: Logger,
+  ) {
+    // Endpoints
+    this.endpoints[ChainIdEnum.kcc] = config.get('BLOCKSCOUT_KCC_URL');
+  }
+
+  endpoints = {};
 
   async fetchAbi(address: Address, chain: ChainId): Promise<AbiItem[] | void> {
-    JSON.stringify({ address, chain });
-    //
+    if (!this.endpoints[chain]) {
+      this.logger.debug(`Chain ${chain} not initialized for ABI fetching`, this.constructor.name);
+      return;
+    }
+
+    const data$ = this.httpService.post(
+      this.endpoints[chain],
+      {
+        variables: { address },
+        query: `query FetchAbi($address: AddressHash) {
+          address(hash: $address) {
+            smartContract {
+              abi
+            }
+          }
+        }`,
+      },
+      {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      },
+    );
+
+    const { data: res }: IBlockScoutResponse = await firstValueFrom(data$);
+
+    const { errors, data } = res;
+
+    if (errors) {
+      this.logger.error(
+        `Failed to retrieve BlockScout ABI ${chain}/${address}`,
+        'BlockScoutService',
+      );
+      return;
+    }
+
+    try {
+      return JSON.parse(data.address.smartContract.abi);
+    } catch {
+      // Fail Gracefully. Maybe it was never meant to be...
+      return;
+    }
   }
 }

4f7d99c7eb6fee9386c9d368ac42df618f3c3fa2
Reed <86171134+not-reed@users.noreply.github.com>
2022-05-25 14:04:09 -0700

    [ID-4361] - Platform Links Required (#1423)
    * [ID-4361] - Platform Links Required
    
    * eslint

--- apps/integration_service/src/framework/platforms/CheesecakeSwap.ts
+++ apps/integration_service/src/framework/platforms/CheesecakeSwap.ts
@@ -20,6 +20,9 @@ export class CheesecakeSwap extends RootPlatform {
     this.registerMeta({
       name: this.constructor.name,
       slug: this.constructor.name,
+      links: {
+        url: 'https://cheesecakeswap.com/',
+      },
     });
 
     await this.registerProtocol<IMasterChefMeta>(MasterChef, {

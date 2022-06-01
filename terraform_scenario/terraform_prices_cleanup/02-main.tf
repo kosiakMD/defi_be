dfb126348ae090e052edf19c503c8c7195d73508
IgorByk <97164174+IgorByk@users.noreply.github.com>
2022-06-01 15:06:23 +0300

    Igorb/feature/lambdas vpc (#1477)
    * Lambda VPC
    
    * Lambda VPC
    
    * Lambda VPC

--- terraform_scenario/terraform_prices_cleanup/02-main.tf
+++ terraform_scenario/terraform_prices_cleanup/02-main.tf
@@ -7,6 +7,7 @@ module "lambda" {
   lambda_options_memory_size = "256"
   lambda_options_timeout     = "60"
   schedule                   = var.schedule_minutes
+  vpc                        = var.vpc
   environment_variables = {
     "DB_HOST" = data.aws_ssm_parameter.DB_HOST.value,
     "DB_PORT" = data.aws_ssm_parameter.DB_PORT.value,

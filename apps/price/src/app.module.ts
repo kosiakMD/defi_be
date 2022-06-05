ac1ecd305d3a2aece6efdda0436351309f5d4044
Petro Sidlovskyy <psidlovskyy@defiyield.app>
2022-06-05 16:12:23 +0300

    Revert "TEST COMMIT: To be reverted. Remove tracing from RPC and prices service."
    This reverts commit 5dda8b95c26761b0425c9a905c760331f66c53e1.

--- apps/price/src/app.module.ts
+++ apps/price/src/app.module.ts
@@ -1,11 +1,17 @@
-import { Inject, Module, OnModuleInit } from '@nestjs/common';
+import { HttpTracingModule, TracingModule } from '@narando/nest-xray';
+import { Inject, MiddlewareConsumer, Module, NestModule, OnModuleInit } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { APP_FILTER } from '@nestjs/core';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { WINSTON_MODULE_NEST_PROVIDER, WinstonModule } from 'nest-winston';
 
 import { Logger } from '@app/common/Logger';
 import { getWinstonParams } from '@app/common/Logger/logger.config';
 import configuration from '@app/common/config/configuration';
+import { interceptorsOrder } from '@app/common/interceptors';
+import { AllExceptionsFilter } from '@app/common/interceptors/all-exceptions.filter';
+import { LogRequestMiddleware } from '@app/common/middlewares';
+import { HeadersContextMiddleware } from '@app/common/middlewares/HeadersContext.middleware';
 
 import config from './config';
 import { HealthModule } from './modules/health.module';
@@ -15,6 +21,7 @@ import { PricesModule } from './modules/prices/prices.module';
 @Module({
   imports: [
     ConfigModule.forRoot(configuration(config)),
+    TracingModule.forRoot({ serviceName: 'price-service' }),
     WinstonModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
@@ -35,13 +42,32 @@ import { PricesModule } from './modules/prices/prices.module';
         logging: false,
       }),
     }),
+    HttpTracingModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        timeout: configService.get<number>('HTTP_TIMEOUT') || 60e3,
+        maxRedirects: configService.get<number>('HTTP_MAX_REDIRECTS') || 2,
+      }),
+      inject: [ConfigService],
+    }),
     PricesModule,
     HealthModule,
     LookupModule,
   ].sort(),
+  providers: [
+    ...interceptorsOrder,
+    {
+      provide: APP_FILTER,
+      useClass: AllExceptionsFilter,
+    },
+  ],
 })
-export class AppModule implements OnModuleInit {
+export class AppModule implements OnModuleInit, NestModule {
   constructor(@Inject(WINSTON_MODULE_NEST_PROVIDER) private readonly logger: Logger) {}
+
+  configure(consumer: MiddlewareConsumer): void {
+    consumer.apply(HeadersContextMiddleware, LogRequestMiddleware).forRoutes('*');
+  }
 
   onModuleInit(): void {
     const { ENV, SERVICE_NAME, SERVICE_PORT, SERVICE_HOST } = process.env;

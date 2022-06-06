bc0765f3562ad1d506f5f5c0b6aeb698c518be03
x-ror <16686943+x-ror@users.noreply.github.com>
2022-06-06 10:41:09 +0300

    update

--- apps/integration/src/framework/platforms/BeefyFinance.ts
+++ apps/integration/src/framework/platforms/BeefyFinance.ts
@@ -39,92 +39,92 @@ export class BeefyFinance extends RootPlatform {
       },
     });
 
-    await this.registerProtocol<IBeefyVaultMeta>(BeefyVault, {
-      chain: ChainIdEnum.plg,
-      name: 'BeefyVault',
-      feature: FeatureEnum.staking,
-      address: '0xFcC42a103d620125b580a0B76fD8a58cc1b6b06A', // Randomly chosen vault to fetch ABI from
-      context: {
-        chainEndpoint: 'polygon',
-        vaultEndpoint: 'https://api.beefy.finance/vaults',
-      },
-    });
+    // await this.registerProtocol<IBeefyVaultMeta>(BeefyVault, {
+    //   chain: ChainIdEnum.plg,
+    //   name: 'BeefyVault',
+    //   feature: FeatureEnum.staking,
+    //   address: '0xFcC42a103d620125b580a0B76fD8a58cc1b6b06A', // Randomly chosen vault to fetch ABI from
+    //   context: {
+    //     chainEndpoint: 'polygon',
+    //     vaultEndpoint: 'https://api.beefy.finance/vaults',
+    //   },
+    // });
 
-    await this.registerProtocol<IBeefyVaultMeta>(BeefyVault, {
-      chain: ChainIdEnum.celo,
-      name: 'BeefyVault',
-      feature: FeatureEnum.staking,
-      address: '0xf68c61e3c2f9c48e53391e1fcd2db1f19998151b', // Randomly chosen vault to fetch ABI from
-      context: {
-        chainEndpoint: 'celo',
-        vaultEndpoint: 'https://api.beefy.finance/vaults',
-      },
-    });
+    // await this.registerProtocol<IBeefyVaultMeta>(BeefyVault, {
+    //   chain: ChainIdEnum.celo,
+    //   name: 'BeefyVault',
+    //   feature: FeatureEnum.staking,
+    //   address: '0xf68c61e3c2f9c48e53391e1fcd2db1f19998151b', // Randomly chosen vault to fetch ABI from
+    //   context: {
+    //     chainEndpoint: 'celo',
+    //     vaultEndpoint: 'https://api.beefy.finance/vaults',
+    //   },
+    // });
 
-    await this.registerProtocol<IBeefyVaultMeta>(BeefyVault, {
-      chain: ChainIdEnum.arbi,
-      name: 'BeefyVault',
-      feature: FeatureEnum.staking,
-      address: '0x78AB636351c1C5f117C1442B82d14aB3a92F8464', // Randomly chosen vault to fetch ABI from
-      context: {
-        chainEndpoint: 'arbitrum',
-        vaultEndpoint: 'https://api.beefy.finance/vaults',
-      },
-    });
+    // await this.registerProtocol<IBeefyVaultMeta>(BeefyVault, {
+    //   chain: ChainIdEnum.arbi,
+    //   name: 'BeefyVault',
+    //   feature: FeatureEnum.staking,
+    //   address: '0x78AB636351c1C5f117C1442B82d14aB3a92F8464', // Randomly chosen vault to fetch ABI from
+    //   context: {
+    //     chainEndpoint: 'arbitrum',
+    //     vaultEndpoint: 'https://api.beefy.finance/vaults',
+    //   },
+    // });
 
-    await this.registerProtocol<IBeefyVaultMeta>(BeefyVault, {
-      chain: ChainIdEnum.avax,
-      name: 'BeefyVault',
-      feature: FeatureEnum.staking,
-      address: '0xCeefB07Ad37ff165A0b03DC7C808fD2E2fC77683', // Randomly chosen vault to fetch ABI from
-      context: {
-        chainEndpoint: 'avax',
-        vaultEndpoint: 'https://api.beefy.finance/vaults',
-      },
-    });
+    // await this.registerProtocol<IBeefyVaultMeta>(BeefyVault, {
+    //   chain: ChainIdEnum.avax,
+    //   name: 'BeefyVault',
+    //   feature: FeatureEnum.staking,
+    //   address: '0xCeefB07Ad37ff165A0b03DC7C808fD2E2fC77683', // Randomly chosen vault to fetch ABI from
+    //   context: {
+    //     chainEndpoint: 'avax',
+    //     vaultEndpoint: 'https://api.beefy.finance/vaults',
+    //   },
+    // });
 
-    await this.registerProtocol<IBeefyVaultMeta>(BeefyVault, {
-      chain: ChainIdEnum.cro,
-      name: 'BeefyVault',
-      feature: FeatureEnum.staking,
-      address: '0xBa5041B1c06e8c9cFb5dDB4b82BdC52E41EA5FC5', // Randomly chosen vault to fetch ABI from
-      context: {
-        chainEndpoint: 'cronos',
-        vaultEndpoint: 'https://api.beefy.finance/vaults',
-      },
-    });
+    // await this.registerProtocol<IBeefyVaultMeta>(BeefyVault, {
+    //   chain: ChainIdEnum.cro,
+    //   name: 'BeefyVault',
+    //   feature: FeatureEnum.staking,
+    //   address: '0xBa5041B1c06e8c9cFb5dDB4b82BdC52E41EA5FC5', // Randomly chosen vault to fetch ABI from
+    //   context: {
+    //     chainEndpoint: 'cronos',
+    //     vaultEndpoint: 'https://api.beefy.finance/vaults',
+    //   },
+    // });
 
-    await this.registerProtocol<IBeefyVaultMeta>(BeefyVault, {
-      chain: ChainIdEnum.ftm,
-      name: 'BeefyVault',
-      feature: FeatureEnum.staking,
-      address: '0x185647c55633A5706aAA3278132537565c925078', // Randomly chosen vault to fetch ABI from
-      context: {
-        chainEndpoint: 'fantom',
-        vaultEndpoint: 'https://api.beefy.finance/vaults',
-      },
-    });
+    // await this.registerProtocol<IBeefyVaultMeta>(BeefyVault, {
+    //   chain: ChainIdEnum.ftm,
+    //   name: 'BeefyVault',
+    //   feature: FeatureEnum.staking,
+    //   address: '0x185647c55633A5706aAA3278132537565c925078', // Randomly chosen vault to fetch ABI from
+    //   context: {
+    //     chainEndpoint: 'fantom',
+    //     vaultEndpoint: 'https://api.beefy.finance/vaults',
+    //   },
+    // });
 
-    await this.registerProtocol<IBeefyVaultMeta>(BeefyVault, {
-      chain: ChainIdEnum.harm,
-      name: 'BeefyVault',
-      feature: FeatureEnum.staking,
-      address: '0x6207536011918F1A0D8a53Bc426f4Fd54df2E5a8', // Randomly chosen vault to fetch ABI from
-      context: {
-        chainEndpoint: 'one',
-        vaultEndpoint: 'https://api.beefy.finance/vaults',
-      },
-    });
+    // await this.registerProtocol<IBeefyVaultMeta>(BeefyVault, {
+    //   chain: ChainIdEnum.harm,
+    //   name: 'BeefyVault',
+    //   feature: FeatureEnum.staking,
+    //   address: '0x06A1f520555222758eaE4dA0573351FdaD1e7843', // Randomly chosen vault to fetch ABI from
+    //   context: {
+    //     chainEndpoint: 'one',
+    //     vaultEndpoint: 'https://api.beefy.finance/vaults',
+    //   },
+    // });
 
-    await this.registerProtocol<IBeefyVaultMeta>(BeefyVault, {
-      chain: ChainIdEnum.mriver,
-      name: 'BeefyVault',
-      feature: FeatureEnum.staking,
-      address: '0xc9a509dA14525Ad3710e9448a0839EE2e90E48B1', // Randomly chosen vault to fetch ABI from
-      context: {
-        chainEndpoint: 'moonriver',
-        vaultEndpoint: 'https://api.beefy.finance/vaults',
-      },
-    });
+    // await this.registerProtocol<IBeefyVaultMeta>(BeefyVault, {
+    //   chain: ChainIdEnum.mriver,
+    //   name: 'BeefyVault',
+    //   feature: FeatureEnum.staking,
+    //   address: '0xc9a509dA14525Ad3710e9448a0839EE2e90E48B1', // Randomly chosen vault to fetch ABI from
+    //   context: {
+    //     chainEndpoint: 'moonriver',
+    //     vaultEndpoint: 'https://api.beefy.finance/vaults',
+    //   },
+    // });
   }
 }
--- apps/integration/src/framework/support/EVM/protocols/Yield/BeefyVault.ts
+++ apps/integration/src/framework/support/EVM/protocols/Yield/BeefyVault.ts
@@ -1,3 +1,4 @@
+import { AccountService } from 'apps/integration/src/modules/microservices/account.service';
 import BigNumber from 'bignumber.js';
 import { Cache } from 'cache-manager';
 import { filter, firstValueFrom, mergeMap, toArray } from 'rxjs';
@@ -6,17 +7,14 @@ import { HttpService } from '@nestjs/axios';
 import { CACHE_MANAGER, Inject } from '@nestjs/common';
 import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
 
-import { Address, CurrentPricesPayload, Logger } from '@app/common';
+import { Address, Logger } from '@app/common';
 import { ZERO_ADDRESS } from '@app/common/constant';
-import { CallData } from '@app/common/dto/CallData';
-import { handlePromiseAllSettled } from '@app/common/helpers/promises';
 import { normalizeDecimals } from '@app/common/utils';
 import { DynamicContract } from '@app/common/web3provider/contracts/DynamicContract';
 import { ERC20 } from '@app/common/web3provider/contracts/ERC20';
-import { UniswapV2Pair } from '@app/common/web3provider/contracts/UniswapV2Pair';
 import { MulticallAggregator } from '@app/common/web3provider/multicall.aggregator';
 
-import { AccountService } from '../../../../../modules/microservices/account.service';
+import { AssetsService } from '../../../../../modules/microservices/assets.service';
 import { PriceService } from '../../../../../modules/microservices/price.service';
 import { FeatureEnum } from '../../../enums';
 import { INamedFunctionPredicates, IProtocolMeta, IRootProtocol } from '../../../interfaces';
@@ -72,6 +70,7 @@ export class BeefyVault
   >
   implements IRootProtocol
 {
+  protected accountService: AccountService;
   protected functionPredicates: INamedFunctionPredicates = {
     decimals: () => (item) => item.name === 'decimals',
     balanceOf: () => (item) => item.name === 'balanceOf',
@@ -84,7 +83,7 @@ export class BeefyVault
     protected multicall: MulticallAggregator,
     @Inject(WINSTON_MODULE_NEST_PROVIDER) protected logger: Logger,
     @Inject(CACHE_MANAGER) protected cache: Cache,
-    protected accountService: AccountService,
+    protected assetsService: AssetsService,
     protected priceService: PriceService,
     protected httpService: HttpService,
   ) {
@@ -200,93 +199,114 @@ export class BeefyVault
     return `${contract}.userInfo(${user})`;
   }
 
-  /**
-   * TODO: remove it once assets service done
-   */
-  async updateUniswapLikeTokensData(tokens: any[], prices: CurrentPricesPayload) {
-    const requests = [];
-    for (const token of tokens) {
-      const calls = new Map();
-      if (token.underlyingAssets?.length !== 2) continue;
-      const contract = new UniswapV2Pair(token.address);
-      calls.set(`${token.address}.totalSupply()`, contract.totalSupply());
-      calls.set(`${token.address}.getReserves()`, contract.getReserves());
-      calls.set(`${token.address}.token0()`, contract.token0());
-      calls.set(`${token.address}.token1()`, contract.token1());
-      token.underlyingAssets.forEach((asset) => {
-        const c = new ERC20(asset.address);
-        calls.set(`${asset.address}.totalSupply()`, c.totalSupply());
-      });
-      requests.push(calls);
-    }
+  protected async getTokens(addresses: Address[]): Promise<[Address, ERC20Token][]> {
+    const assets = Array.from(new Set(addresses)).map((address) => ({
+      address,
+      chainId: this.meta.chain,
+    }));
+    const result: any[] = await this.assetsService.getAssetsBulk(assets);
 
-    const responsesRaw = await Promise.allSettled(
-      requests.flatMap((call) => this.multicall.handleInBatches(call, this.meta.chain)),
-    );
-    const [data, errors] = handlePromiseAllSettled(responsesRaw);
-
-    const dataArray: any[] = data.flatMap((callData) => Array.from(callData.entries()));
-    const results: Map<string, CallData> = new Map(dataArray);
-
-    tokens.forEach((token: any) => {
-      if (token.underlyingAssets?.length !== 2 || !results.has(`${token.address}.totalSupply()`))
-        return;
-      const totalSupply = results.get(`${token.address}.totalSupply()`).output.data;
-      const token0Address = results.get(`${token.address}.token0()`).output.data.toLowerCase();
-      const token1Address = results.get(`${token.address}.token1()`).output.data.toLowerCase();
-      const { _reserve0, _reserve1 } = results.get(`${token.address}.getReserves()`).output.data;
-      if (!Number(prices[token0Address]) && !Number(prices[token1Address])) return;
-
-      const underlying0 = token.underlyingAssets.find((a) => a.address === token0Address);
-      const underlying1 = token.underlyingAssets.find((a) => a.address === token1Address);
-
-      const reserve0 = normalizeDecimals(_reserve0.toString(), underlying0.decimals);
-      const reserve1 = normalizeDecimals(_reserve1.toString(), underlying1.decimals);
-
-      // calculate/fill in missing base token prices based on current LP reserves
-      if (!prices[token0Address]) {
-        prices[token0Address] = (reserve1 * Number(prices[token1Address])) / reserve0;
-      }
-
-      if (!prices[token1Address]) {
-        prices[token1Address] = (reserve0 * Number(prices[token0Address])) / reserve1;
-      }
-
-      // calculate/fill the LP token price into the price array
-      const tvl0 = new BigNumber(reserve0 * Number(prices[token0Address]));
-      const tvl1 = new BigNumber(reserve1 * Number(prices[token1Address]));
-
-      token.totalSupply = normalizeDecimals(totalSupply, token.decimals);
-
-      prices[token.address] = new BigNumber(tvl0.plus(tvl1)) //
-        .div(token.totalSupply)
-        .toNumber();
-
-      token.underlyingAssets.forEach((u) => {
-        u.totalSupply = normalizeDecimals(
-          results.get(`${u.address}.totalSupply()`).output.data,
-          u.decimals,
-        );
-
-        u.reserve = normalizeDecimals(
-          (u.reserve = u.positionInPool === 0 ? _reserve0 : _reserve1).toString(),
-          u.decimals,
-        );
-      });
+    return result.map((token) => {
+      return [
+        token.address.toLowerCase(),
+        {
+          ...token,
+          address: token.address.toLowerCase(),
+          underlying: token.underlying?.map((u): ERC20Token => {
+            return result.find((x) => x.address === u.address);
+          }),
+        },
+      ];
     });
-    return tokens;
   }
 
-  protected async updateTokenData(
-    tokens: any[],
-    prices: CurrentPricesPayload,
-  ): Promise<ERC20Token[]> {
-    try {
-      return await this.updateUniswapLikeTokensData(tokens, prices);
-    } catch (err) {
-      // TODO: delete this block, Prices should come from asset service, not calculated here
-      this.logger.error(err.message, err.stack, 'EVMCore');
-      return tokens;
-    }
-  }
+  // /**
+  //  * TODO: remove it once assets service done
+  //  */
+  // async updateUniswapLikeTokensData(tokens: any[], prices: CurrentPricesPayload) {
+  //   const requests = [];
+  //   for (const token of tokens) {
+  //     const calls = new Map();
+  //     if (token.underlyingAssets?.length !== 2) continue;
+  //     const contract = new UniswapV2Pair(token.address);
+  //     calls.set(`${token.address}.totalSupply()`, contract.totalSupply());
+  //     calls.set(`${token.address}.getReserves()`, contract.getReserves());
+  //     calls.set(`${token.address}.token0()`, contract.token0());
+  //     calls.set(`${token.address}.token1()`, contract.token1());
+  //     token.underlyingAssets.forEach((asset) => {
+  //       const c = new ERC20(asset.address);
+  //       calls.set(`${asset.address}.totalSupply()`, c.totalSupply());
+  //     });
+  //     requests.push(calls);
+  //   }
+
+  //   const responsesRaw = await Promise.allSettled(
+  //     requests.flatMap((call) => this.multicall.handleInBatches(call, this.meta.chain)),
+  //   );
+  //   const [data, errors] = handlePromiseAllSettled(responsesRaw);
+
+  //   const dataArray: any[] = data.flatMap((callData) => Array.from(callData.entries()));
+  //   const results: Map<string, CallData> = new Map(dataArray);
+
+  //   tokens.forEach((token: any) => {
+  //     if (token.underlyingAssets?.length !== 2 || !results.has(`${token.address}.totalSupply()`))
+  //       return;
+  //     const totalSupply = results.get(`${token.address}.totalSupply()`).output.data;
+  //     const token0Address = results.get(`${token.address}.token0()`).output.data.toLowerCase();
+  //     const token1Address = results.get(`${token.address}.token1()`).output.data.toLowerCase();
+  //     const { _reserve0, _reserve1 } = results.get(`${token.address}.getReserves()`).output.data;
+  //     if (!Number(prices[token0Address]) && !Number(prices[token1Address])) return;
+
+  //     const underlying0 = token.underlyingAssets.find((a) => a.address === token0Address);
+  //     const underlying1 = token.underlyingAssets.find((a) => a.address === token1Address);
+
+  //     const reserve0 = normalizeDecimals(_reserve0.toString(), underlying0.decimals);
+  //     const reserve1 = normalizeDecimals(_reserve1.toString(), underlying1.decimals);
+
+  //     // calculate/fill in missing base token prices based on current LP reserves
+  //     if (!prices[token0Address]) {
+  //       prices[token0Address] = (reserve1 * Number(prices[token1Address])) / reserve0;
+  //     }
+
+  //     if (!prices[token1Address]) {
+  //       prices[token1Address] = (reserve0 * Number(prices[token0Address])) / reserve1;
+  //     }
+
+  //     // calculate/fill the LP token price into the price array
+  //     const tvl0 = new BigNumber(reserve0 * Number(prices[token0Address]));
+  //     const tvl1 = new BigNumber(reserve1 * Number(prices[token1Address]));
+
+  //     token.totalSupply = normalizeDecimals(totalSupply, token.decimals);
+
+  //     prices[token.address] = new BigNumber(tvl0.plus(tvl1)) //
+  //       .div(token.totalSupply)
+  //       .toNumber();
+
+  //     token.underlyingAssets.forEach((u) => {
+  //       u.totalSupply = normalizeDecimals(
+  //         results.get(`${u.address}.totalSupply()`).output.data,
+  //         u.decimals,
+  //       );
+
+  //       u.reserve = normalizeDecimals(
+  //         (u.reserve = u.positionInPool === 0 ? _reserve0 : _reserve1).toString(),
+  //         u.decimals,
+  //       );
+  //     });
+  //   });
+  //   return tokens;
+  // }
+
+  // protected async updateTokenData(
+  //   tokens: any[],
+  //   prices: CurrentPricesPayload,
+  // ): Promise<ERC20Token[]> {
+  //   try {
+  //     return await this.updateUniswapLikeTokensData(tokens, prices);
+  //   } catch (err) {
+  //     // TODO: delete this block, Prices should come from asset service, not calculated here
+  //     this.logger.error(err.message, err.stack, 'EVMCore');
+  //     return tokens;
+  //   }
+  // }
 }

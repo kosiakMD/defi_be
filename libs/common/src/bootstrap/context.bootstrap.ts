2c1b39a06621c5fa9acff2d7d0729e2e81a65b44
kosiakmd <keleborn.mail@gmail.com>
2022-06-08 13:42:52 +0300

    ID-4555 clean redundant

--- libs/common/src/bootstrap/context.bootstrap.ts
+++ libs/common/src/bootstrap/context.bootstrap.ts
@@ -1,13 +1,9 @@
-// import * as childProcess from 'child_process';
 import { NestExpressApplication } from '@nestjs/platform-express';
 
 import { withContext } from '@app/common/middlewares/withContext';
 
 export function initContext(app: NestExpressApplication): NestExpressApplication {
   app.use(withContext);
-
-  // const revision = childProcess.execSync('git rev-parse HEAD').toString().trim();
-  // console.log(revision);
 
   return app;
 }

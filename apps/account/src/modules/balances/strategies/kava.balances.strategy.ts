89961420ad5c3de5442d9ccb058f9b9306536c44
Andrii Tymchenko <16686943+x-ror@users.noreply.github.com>
2022-06-10 18:03:40 +0300

    updated http service for cosmos-hub (#1575)

--- apps/account/src/modules/balances/strategies/kava.balances.strategy.ts
+++ apps/account/src/modules/balances/strategies/kava.balances.strategy.ts
@@ -1,4 +1,4 @@
-import { HttpService } from '@nestjs/axios';
+import { HttpService } from '@nestjs/common';
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 

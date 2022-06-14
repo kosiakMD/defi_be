dd412a7196087fef978721266cde43cca2fa4cc3
ArtemBondarenko <artemb@defiyield.app>
2022-06-14 10:53:40 +0200

    account build fix

--- apps/account/src/modules/balances/strategies/cosmos-hub.balances.strategy.ts
+++ apps/account/src/modules/balances/strategies/cosmos-hub.balances.strategy.ts
@@ -1,6 +1,6 @@
 import { firstValueFrom, map } from 'rxjs';
 
-import { HttpService } from '@nestjs/axios';
+import { HttpService } from '@nestjs/common';
 
 import { isBech32LikeAddress } from '@app/common/utils';
 

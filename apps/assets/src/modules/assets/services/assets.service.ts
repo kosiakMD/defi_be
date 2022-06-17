56ef8df6fe588a23d5287ff6092e2034e8b9e2b0
defi-sidlovskyy <106627658+defi-sidlovskyy@users.noreply.github.com>
2022-06-17 10:30:53 +0300

    Feature/id 4554 balances integrate asset service (#1604)
    * Small but important fixes
    
    * Add API to get accounted assets

--- apps/assets/src/modules/assets/services/assets.service.ts
+++ apps/assets/src/modules/assets/services/assets.service.ts
@@ -6,6 +6,8 @@ import { ConfigService } from '@nestjs/config';
 import { InjectRepository } from '@nestjs/typeorm';
 import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
 
+import { ChainId } from '@app/common';
+import { CacheService } from '@app/common/services/cache.service';
 import { CrudService } from '@app/common/services/crud.service';
 import { isSomeAddress } from '@app/common/utils';
 
@@ -38,6 +40,7 @@ export class AssetsService extends CrudService<AssetsRepository> {
   constructor(
     @Inject(WINSTON_MODULE_NEST_PROVIDER) private readonly logger: LoggerService,
     @InjectQueue(QueueName.ASSETS) private readonly assetsQueue: Queue,
+    private readonly cacheService: CacheService,
     private readonly assetsRepository: AssetsCachedRepository,
     @InjectRepository(AssetsHistoricalPriceRepository)
     private readonly assetsHistoricalPriceRepository: AssetsHistoricalPriceRepository,
@@ -184,6 +187,22 @@ export class AssetsService extends CrudService<AssetsRepository> {
     }, new Map<number, AssetHistoricalPriceDto[]>());
     assets.forEach((asset) => (asset.historicalPrices = historicalPrices.get(asset.id) || []));
     return assets;
+  }
+
+  public async getAccountedAssetsByChain(chainId: ChainId): Promise<AssetDto[]> {
+    const assets = await this.cacheService.getOrLoad(
+      `assets_service_balances_assets_${chainId}`,
+      () => this.assetsRepository.findTrackedAssetsByChain(chainId),
+      {
+        ttl: 15 * 60, // 15 minutes
+      },
+    );
+    // NOTE: We don't care about underlying and special assets as we won't show them in balances
+    const assetsForBalances = assets.filter(({ isNotAccounted }) => !isNotAccounted);
+    const dtos = mapAssetsToPlain(assetsForBalances);
+    const dtosWithPrices = await this.addPrices(dtos);
+    // NOTE: Only return assets that have prices as others we don't show on balances
+    return dtosWithPrices.filter(({ price }) => !!price);
   }
 
   public async search(searchParams: SearchParams): Promise<SearchResultsEntryDto[]> {

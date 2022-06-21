d24048f940d6b892ca298765767f9a3eb7027871
not-reed <reed@degen.watch>
2022-06-21 14:29:08 -0700

    Added simple interest bearing token

--- apps/integration/src/framework/platforms/AaveV2.ts
+++ apps/integration/src/framework/platforms/AaveV2.ts
@@ -5,6 +5,7 @@ import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
 import { ChainIdEnum, Logger } from '@app/common';
 
 import { AaveV2Lending, IAaveV2Meta } from '../support/EVM/protocols/Lending/AaveV2Lending';
+import { InterestBearingToken } from '../support/EVM/protocols/Yield/InterestBearingToken';
 import { RootPlatform } from '../support/RootPlatform';
 import { FeatureEnum } from '../support/enums';
 
@@ -27,28 +28,49 @@ export class AaveV2 extends RootPlatform {
       },
     });
 
-    await this.registerProtocol<IAaveV2Meta>(AaveV2Lending, {
+    // await this.registerProtocol<IAaveV2Meta>(AaveV2Lending, {
+    //   chain: ChainIdEnum.eth,
+    //   name: 'Lending - Aave',
+    //   feature: FeatureEnum.lending,
+    //   address: '0x7d2768de32b0b80b7a3454c06bdac94a69ddc7a9',
+    //   incentives: '0xd784927Ff2f95ba542BfC824c8a8a98F3495f6b5',
+    // });
+
+    // await this.registerProtocol<IAaveV2Meta>(AaveV2Lending, {
+    //   chain: ChainIdEnum.plg,
+    //   name: 'Lending - Aave',
+    //   feature: FeatureEnum.lending,
+    //   address: '0x8dff5e27ea6b7ac08ebfdf9eb090f32ee9a30fcf',
+    //   incentives: '0x357D51124f59836DeD84c8a1730D72B749d8BC23',
+    // });
+
+    // await this.registerProtocol<IAaveV2Meta>(AaveV2Lending, {
+    //   chain: ChainIdEnum.avax,
+    //   name: 'Lending - Aave',
+    //   feature: FeatureEnum.lending,
+    //   address: '0x4F01AeD16D97E3aB5ab2B501154DC9bb0F1A5A2C',
+    //   incentives: '0x01D83Fe6A10D2f2B7AF17034343746188272cAc9',
+    // });
+
+    // Stake a single token with an underlying balance
+    // this is the same as Beefy X 1
+    // poolslist => just the token
+    // user balance => balance of main address * exchange rate (underlying could be LP token, etc)
+    // text address: 0x4ca31d938bc2d23e68eab871e5a0a02019f8dce6
+    await this.registerProtocol(InterestBearingToken, {
       chain: ChainIdEnum.eth,
-      name: 'Lending - Aave',
-      feature: FeatureEnum.lending,
-      address: '0x7d2768de32b0b80b7a3454c06bdac94a69ddc7a9',
-      incentives: '0xd784927Ff2f95ba542BfC824c8a8a98F3495f6b5',
+      name: 'Safety Module',
+      feature: FeatureEnum.staking,
+      address: '0x4da27a545c0c5b758a6ba100e3a049001de870f5',
+      // underlying: '0x7Fc66500c84A76Ad7e9c93437bFc5Ac33E2DDaE9',
     });
 
-    await this.registerProtocol<IAaveV2Meta>(AaveV2Lending, {
-      chain: ChainIdEnum.plg,
-      name: 'Lending - Aave',
-      feature: FeatureEnum.lending,
-      address: '0x8dff5e27ea6b7ac08ebfdf9eb090f32ee9a30fcf',
-      incentives: '0x357D51124f59836DeD84c8a1730D72B749d8BC23',
-    });
-
-    await this.registerProtocol<IAaveV2Meta>(AaveV2Lending, {
-      chain: ChainIdEnum.avax,
-      name: 'Lending - Aave',
-      feature: FeatureEnum.lending,
-      address: '0x4F01AeD16D97E3aB5ab2B501154DC9bb0F1A5A2C',
-      incentives: '0x01D83Fe6A10D2f2B7AF17034343746188272cAc9',
+    await this.registerProtocol(InterestBearingToken, {
+      chain: ChainIdEnum.eth,
+      name: 'Safety Module',
+      feature: FeatureEnum.staking,
+      address: '0xa1116930326d21fb917d5a27f1e9943a9595fb47',
+      // underlying: '0x7Fc66500c84A76Ad7e9c93437bFc5Ac33E2DDaE9',
     });
   }
 }
---
+++ apps/integration/src/framework/support/EVM/protocols/Yield/InterestBearingToken.ts
@@ -0,0 +1,131 @@
+import { AssetService } from 'apps/integration/src/modules/microservices/asset.service';
+import { Cache } from 'cache-manager';
+
+import { CACHE_MANAGER, Inject } from '@nestjs/common';
+import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
+
+import { Logger } from '@app/common';
+import { normalizeDecimals } from '@app/common/utils';
+import { ERC20 } from '@app/common/web3provider/contracts/ERC20';
+import { MulticallAggregator } from '@app/common/web3provider/multicall.aggregator';
+
+import { INamedFunctionPredicates, IProtocolMeta } from '../../../interfaces';
+import { BaseWithTokens } from '../../../interfaces/new.interfaces';
+import {
+  IRewardTokenMinimal,
+  IRewardTokenOpportunity,
+  IRewardTokenUserEntry,
+} from '../../../interfaces/tokens.rewarded.interface';
+import {
+  ISupplyTokenMinimal,
+  ISupplyTokenOpportunity,
+  ISupplyTokenUserEntry,
+} from '../../../interfaces/tokens.supplied.interface';
+import { AbiService } from '../../AbiModule/AbiService';
+import { SingleContractProtocol } from '../../SingleContractProtocol';
+
+export interface InterestBearingTokenMetaInterface extends IProtocolMeta {
+  address: '';
+  context: {
+    // badPools?: number[];
+    // poolLength: number;
+  };
+}
+
+type IInterestBearingTokenMinimal = BaseWithTokens<ISupplyTokenMinimal, IRewardTokenMinimal>;
+
+type IInterestBearingTokenOpportunity = BaseWithTokens<
+  ISupplyTokenOpportunity,
+  IRewardTokenOpportunity
+>;
+
+type IInterestBearingTokenUser = BaseWithTokens<ISupplyTokenUserEntry, IRewardTokenUserEntry>;
+/**
+ * Single interest bearing token with underlying balance
+ *
+ * In general staking contract is actually a ERC20 token itself.
+ * so Staked Token and Reward Token are often the same token (but not always)
+ * and user balance is # of stakedTokens * some exchange rate
+ */
+export class InterestBearingToken extends SingleContractProtocol<
+  IInterestBearingTokenMinimal,
+  IInterestBearingTokenOpportunity,
+  IInterestBearingTokenUser,
+  InterestBearingTokenMetaInterface
+> {
+  constructor(
+    protected abiService: AbiService,
+    protected multicall: MulticallAggregator,
+    @Inject(WINSTON_MODULE_NEST_PROVIDER) protected logger: Logger,
+    @Inject(CACHE_MANAGER) protected cache: Cache,
+    protected assetService: AssetService,
+  ) {
+    super();
+  }
+
+  // function predicates to find staked & reward tokens
+  // auto compound true/false flag
+  // overridable exchange rate function
+  protected compounding = false;
+
+  protected functionPredicates: INamedFunctionPredicates = {
+    stakedToken: () => (item) => item.name === 'STAKED_TOKEN',
+    rewardToken: () => (item) => item.name === 'REWARD_TOKEN',
+    getStakedBalance: () => (item) => item.name === 'balanceOf',
+    getRewardBalance: () => (item) => item.name === 'getTotalRewardsBalance',
+  };
+
+  protected async fetchOpportunityData(context: {
+    [key: string]: any;
+  }): Promise<IInterestBearingTokenMinimal[]> {
+    return [
+      {
+        id: this.meta.address,
+        chain: this.meta.chain,
+        feature: this.meta.feature,
+        supply: {
+          token: { address: context.stakedToken },
+          // totalSupplied: context.totalSupply.toString(), // staked token total supply * exchange rate
+        },
+        reward: {
+          token: { address: context.rewardToken },
+        },
+      },
+    ];
+  }
+
+  protected async fetchUserData(
+    address: string,
+    pools: IInterestBearingTokenOpportunity[],
+  ): Promise<IInterestBearingTokenUser[]> {
+    const [pool] = pools;
+    const contract = this.getMainContract();
+    const [stakedBalance, rewardBalance] = await this.multicall.callArray(
+      [
+        contract.createCall(this.functions.getStakedBalance, address),
+        contract.createCall(this.functions.getRewardBalance, address),
+      ],
+      this.meta.chain,
+    );
+    const stakedAmount = normalizeDecimals(stakedBalance, pool.supply.token.decimals);
+    const rewardAmount = normalizeDecimals(rewardBalance, pool.reward.token.decimals);
+
+    return [
+      {
+        ...pool,
+        supply: {
+          ...pool.supply,
+          amount: stakedAmount,
+          value: stakedAmount * pool.supply.token.price,
+        },
+        reward: {
+          ...pool.reward,
+          amount: rewardAmount,
+          value: rewardAmount * pool.reward.token.price,
+          apr: null,
+          apy: null,
+        },
+      },
+    ];
+  }
+}

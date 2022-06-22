e45b5ae288d977045d925ff44a757f126ba496c8
not-reed <reed@degen.watch>
2022-06-22 14:20:08 -0700

    restore disabled aave protocols

--- apps/integration/src/framework/platforms/AaveV2.ts
+++ apps/integration/src/framework/platforms/AaveV2.ts
@@ -28,29 +28,29 @@ export class AaveV2 extends RootPlatform {
       },
     });
 
-    // await this.registerProtocol<IAaveV2Meta>(AaveV2Lending, {
-    //   chain: ChainIdEnum.eth,
-    //   name: 'Lending - Aave',
-    //   feature: FeatureEnum.lending,
-    //   address: '0x7d2768de32b0b80b7a3454c06bdac94a69ddc7a9',
-    //   incentives: '0xd784927Ff2f95ba542BfC824c8a8a98F3495f6b5',
-    // });
+    await this.registerProtocol<IAaveV2Meta>(AaveV2Lending, {
+      chain: ChainIdEnum.eth,
+      name: 'Lending - Aave',
+      feature: FeatureEnum.lending,
+      address: '0x7d2768de32b0b80b7a3454c06bdac94a69ddc7a9',
+      incentives: '0xd784927Ff2f95ba542BfC824c8a8a98F3495f6b5',
+    });
 
-    // await this.registerProtocol<IAaveV2Meta>(AaveV2Lending, {
-    //   chain: ChainIdEnum.plg,
-    //   name: 'Lending - Aave',
-    //   feature: FeatureEnum.lending,
-    //   address: '0x8dff5e27ea6b7ac08ebfdf9eb090f32ee9a30fcf',
-    //   incentives: '0x357D51124f59836DeD84c8a1730D72B749d8BC23',
-    // });
+    await this.registerProtocol<IAaveV2Meta>(AaveV2Lending, {
+      chain: ChainIdEnum.plg,
+      name: 'Lending - Aave',
+      feature: FeatureEnum.lending,
+      address: '0x8dff5e27ea6b7ac08ebfdf9eb090f32ee9a30fcf',
+      incentives: '0x357D51124f59836DeD84c8a1730D72B749d8BC23',
+    });
 
-    // await this.registerProtocol<IAaveV2Meta>(AaveV2Lending, {
-    //   chain: ChainIdEnum.avax,
-    //   name: 'Lending - Aave',
-    //   feature: FeatureEnum.lending,
-    //   address: '0x4F01AeD16D97E3aB5ab2B501154DC9bb0F1A5A2C',
-    //   incentives: '0x01D83Fe6A10D2f2B7AF17034343746188272cAc9',
-    // });
+    await this.registerProtocol<IAaveV2Meta>(AaveV2Lending, {
+      chain: ChainIdEnum.avax,
+      name: 'Lending - Aave',
+      feature: FeatureEnum.lending,
+      address: '0x4F01AeD16D97E3aB5ab2B501154DC9bb0F1A5A2C',
+      incentives: '0x01D83Fe6A10D2f2B7AF17034343746188272cAc9',
+    });
 
     // Stake a single token with an underlying balance
     // poolslist => just the token

988e5cb46c4552578156cb53080642c3ea175a7e
stelyang <stelyang@defiyield.app>
2022-06-23 15:32:25 +0300

    [ID-4744]: hotfix protocols loading

--- apps/integration_service/src/framework/support/RootPlatform.ts
+++ apps/integration_service/src/framework/support/RootPlatform.ts
@@ -22,7 +22,6 @@ import {
   IWalletUserEntry,
 } from './interfaces';
 
-// TODO Make Meta generic & extend
 export abstract class RootPlatform implements IRootPlatform {
   protected abstract logger: Logger;
   protected abstract moduleRef: ModuleRef;
@@ -39,16 +38,54 @@ export abstract class RootPlatform implements IRootPlatform {
   // To get a decluttered list from defilama
   // $ curl https://api.llama.fi/protocols | jq '.[] | to_entries | map(select(.key | in({ name: true, twitter: true, url: true, logo: true }) )) | from_entries'
   protected protocols: Set<IRootProtocol> = new Set();
+  protected protocolErrors: Map<IRootProtocol, Error> = new Map();
+
   protected async registerProtocol<TProtocolMeta extends IProtocolMeta = IProtocolMeta>(
     protocol: ClassConstructor<IRootProtocol>,
     meta: TProtocolMeta,
   ) {
+    // Register protocol with regular dependency injection enabled
     const instance = await this.moduleRef.create(protocol);
+
+    // Assign metadata to protocol
     instance.registerMeta(meta);
+
+    // Run initialization if required.
+    // this sets all required function predicates and ABI items
+    // this is needed to happen infrequently (currently once at boot)
+    // but isn't strictly required to register as the 'meta' has all the info
+    // needed to display the list of protocols.
+    // Pros of including here
+    //   - runs once at start and is done
+    //   - central for all protocols/platforms
+    //   - if a protocol fails to load for any reason it can automatically be removed from the protocols list
+    // Cons
+    //   - slow to start
+    //   - requests many ABI's at once so higher chance of rate limits (assuming protocol list is the first request)
+    //   - means registerProtocol needs to be async
+    // Possible solution:
+    // seperate 'register' and 'boot' actions. Register is called to list
+    // the protocol list, however doesn't touch the ABI's or predicates etc.
+    // boot is run every getPools or getUserData, however it will usually
+    // be loaded from cache so performance impact to users should be minimal
+    // + faster startup times, faster protocols-list time
+    // - potentially slower getUserData time
+
+    // All protocols would be listed, and failed ones would display an error
+    // (or retry_ when accessed instead of simply being absent after boot
+
+    // TODO: initialize could be replaced with proper nestjs lifecycle hooks
+    // https://docs.nestjs.com/fundamentals/lifecycle-events#lifecycle-events-1
     if (instance.initialize) {
       try {
+        this.logger.log(
+          `Initializing: ${meta.name} Chain: ${meta.chain}`,
+          `${this.constructor.name}/${instance.constructor.name}`,
+        );
+
         await instance.initialize();
       } catch (err) {
+        this.protocolErrors.set(instance, err);
         this.logger.error(err.message, err.stack, 'RootPlatform');
         return;
       }
@@ -64,14 +101,23 @@ export abstract class RootPlatform implements IRootPlatform {
     this.protocols.add(instance);
   }
 
+  // if seperated as commented above, register could be sync and
+  // registrationLocked would not be required
   async initialize() {
     await this.register();
     this.registrationLocked = true;
   }
 
+  getSupportedChains() {
+    return this.getMeta().features.map((feature) => feature.chain.id);
+  }
+
   getMeta(): IPlatformMeta {
     // Loop through all supported protocols
     // dedupe & merge
+    // TODO: Comment that out as cannot switch log level (to be reverted)
+    // this.logger.debug(`Start getting meta for: ${this.meta.name}`);
+
     const features = new Map<ChainId, Set<FeatureEnum>>();
     this.protocols.forEach((protocol) => {
       const meta = protocol.getMeta();
@@ -91,26 +137,65 @@ export abstract class RootPlatform implements IRootPlatform {
       })),
       links: this.meta.links || {},
     };
+
+    // TODO: Comment that out as cannot switch log level (to be reverted)
+    // this.logger.debug(`Finish getting meta for: ${this.meta.name}`);
+  }
+  private async validatedProtocols(): Promise<[Set<IRootProtocol<IProtocolMeta>>, Error[]]> {
+    const errors: Error[] = [];
+
+    if (!this.protocolErrors.size) return [this.protocols, errors];
+
+    for (const [protocol] of this.protocolErrors) {
+      try {
+        await protocol.initialize();
+        this.protocolErrors.delete(protocol);
+        this.protocols.add(protocol);
+      } catch (err) {
+        errors.push(err);
+      }
+    }
+    return [this.protocols, errors];
   }
 
   async getUsersData(chains: ChainId[], addresses: Address[]): Promise<IUserDataPlatformResponse> {
+    const [protocols, errors] = await this.validatedProtocols(); // TODO: for performance, we could only retry failed protocols during sync
+
+    this.logger.log(`Start getting user data for: ${this.meta.name}`);
+
     const promises: Promise<IChainGroupedWallet>[] = [];
-    const errors: Error[] = [];
     const supportedChains = new Set();
-    this.protocols.forEach((protocol) => {
+    protocols.forEach((protocol) => {
       const { chain, list: features } = protocol.getMeta();
       supportedChains.add(chain.id);
 
       if (!chains.includes(chain.id) || !protocol.getUsersData) {
         return;
       }
+
       const validAddressesForChain = keepAddressesByChainId(addresses, chain.id);
 
+      if (validAddressesForChain.length !== addresses.length) {
+        this.logger.warn(
+          `${addresses.length - validAddressesForChain.length} Addresses removed from ${
+            this.meta.name
+          }`,
+        );
+      }
+
+      const started = Date.now();
       if (validAddressesForChain?.length) {
         promises.push(
           protocol
             .getUsersData(validAddressesForChain)
             .then(({ data: wallets, errors: userErrors }) => {
+              this.logger.log({
+                message: `Protocol data loaded`,
+                platform: this.meta.name,
+                chainId: chain.id,
+                address: validAddressesForChain,
+                execution: Date.now() - started,
+              });
               errors.push(...userErrors);
               return { chain, features, wallets, errors };
             }),
@@ -125,7 +210,20 @@ export abstract class RootPlatform implements IRootPlatform {
       }
     });
 
-    const resolvedProtocols = await Promise.all(promises);
+    this.logger.log(`Trying to get user data for: ${this.meta.name}`);
+
+    const resolvedProtocolsResults = await Promise.allSettled(promises);
+    const resolvedProtocols = [];
+    resolvedProtocolsResults.forEach((r) => {
+      if (r.status === 'fulfilled') {
+        resolvedProtocols.push(r.value);
+      } else {
+        errors.push(r.reason);
+      }
+    });
+
+    this.logger.log(`Formatting fetched data for: ${this.meta.name}`);
+
     const wallets = addresses.map((address) => {
       const chainData = chains.map((chain) =>
         this.mergeUserProtocolDataPerChain(address, chain, resolvedProtocols),
@@ -138,14 +236,18 @@ export abstract class RootPlatform implements IRootPlatform {
       };
     });
 
+    this.logger.log(`Finish getting user data for: ${this.meta.name}`);
+
     return { data: wallets, errors };
   }
 
   async getPoolData(chains: ChainId[]): Promise<IPoolDataPlatformResponse> {
+    const [protocols, errors] = await this.validatedProtocols();
+    this.logger.log(`Start getting pool data: ${this.meta.name}`);
+
     const promises = [];
-    const errors: Error[] = [];
     const supportedChains = new Set();
-    this.protocols.forEach((protocol) => {
+    protocols.forEach((protocol) => {
       const { chain } = protocol.getMeta();
       supportedChains.add(chain.id);
       if (chains.includes(chain.id)) {
@@ -161,12 +263,14 @@ export abstract class RootPlatform implements IRootPlatform {
     });
 
     const protocolResults = await Promise.allSettled(promises);
-    const protocols: IWalletOpportunity[] = [];
+    const userProtocols: IWalletOpportunity[] = [];
+
+    this.logger.log(`Start formatting pool data: ${this.meta.name}`);
 
     protocolResults.forEach((protocol) => {
       switch (protocol.status) {
         case 'fulfilled':
-          protocols.push(...protocol.value.data);
+          userProtocols.push(...protocol.value.data);
           errors.push(...protocol.value.errors);
           break;
         case 'rejected':
@@ -175,10 +279,12 @@ export abstract class RootPlatform implements IRootPlatform {
       }
     });
 
-    return { data: protocols, errors };
+    this.logger.log(`Finishing getting pool data: ${this.meta.name}`);
+    return { data: userProtocols, errors };
   }
 
   async cachePoolData(chains: ChainId[]) {
+    this.logger.log(`Start caching pool data: ${this.meta.name}`);
     const promises = [];
     this.protocols.forEach((protocol) => {
       const { chain } = protocol.getMeta();
@@ -189,8 +295,10 @@ export abstract class RootPlatform implements IRootPlatform {
 
     const resolvedProtocols = await Promise.allSettled(promises);
 
+    this.logger.log(`Pool data cached: ${this.meta.name}`);
+
     const protocols = [];
-    const errors = [];
+    const errors: Error[] = [];
     resolvedProtocols.forEach((protocol) => {
       switch (protocol.status) {
         case 'fulfilled':
@@ -262,6 +370,10 @@ export abstract class RootPlatform implements IRootPlatform {
    * Also builds a list of all supported features, and calculates
    * chain total
    *
+   * TODO: this was formatted to match as closely to the v2 format as possible
+   * however that compatibility causes extra complexity here. When migrating to /v3
+   * api, I think this could be much simplified
+   *
    * @param user
    * @param chain
    * @param resolvedProtocols
@@ -272,6 +384,9 @@ export abstract class RootPlatform implements IRootPlatform {
     chain: ChainIdEnum,
     resolvedProtocols: IChainGroupedWallet[],
   ): IChainUserEntry {
+    // TODO: Comment that out as cannot switch log level (to be reverted)
+    // this.logger.debug(`Start merging data: ${this.meta.name}`);
+
     const positions: IWalletUserEntry[] = [];
     const features = new Set<Partial<FeatureEnum>>();
 
@@ -290,6 +405,9 @@ export abstract class RootPlatform implements IRootPlatform {
         positionsByFeature[feature] = [];
       }
     });
+
+    // TODO: Comment that out as cannot switch log level (to be reverted)
+    // this.logger.debug(`Finish merging data: ${this.meta.name}`);
 
     return {
       // group positions by feature  { staking: [....], lending: [...] }
